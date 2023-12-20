f6f67abc07f18c05202113071ce0be49ad3c5f05
boomchanotai <boom9387@hotmail.com>
2023-12-20 23:38:12 +0700

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,20 @@
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
-  extend: {
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-      "ibm-plex-sans-thai": ["IBM Plex Sans Thai", "sans-serif"],
-    },
-    colors: {
-      primary: "#08878E",
-      "primary-variant": "#83C3C6",
-      secondary: "#C81425",
-      "johnjud-purple": "#3D0D40",
-      "johnjud-gray": "#808086",
-      "johnjud-yellow": "#FFFF31",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        "ibm-plex-sans-thai": ["IBM Plex Sans Thai", "sans-serif"],
+      },
+      colors: {
+        primary: "#08878E",
+        "primary-variant": "#83C3C6",
+        secondary: "#C81425",
+        "johnjud-purple": "#3D0D40",
+        "johnjud-gray": "#808086",
+        "johnjud-yellow": "#FFFF31",
+      },
     },
   },
   plugins: [],
