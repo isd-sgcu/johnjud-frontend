d4f8da4e128c90f3f5960baf048e757e2ef752a4
boomchanotai <boom9387@hotmail.com>
2023-12-20 14:23:31 +0700

    feat: add background image

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,19 @@
+import background from "../assets/background/background.png";
+
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <div className="min-h-screen flex flex-col justify-center items-center">{children}</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${background})`
+      }}
+      className="min-h-screen flex flex-col justify-center items-center bg-cover bg-center">
+      {children}
+    </div>
+  );
 };
 
 export default MainLayout;
