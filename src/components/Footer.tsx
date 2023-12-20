5ff96fc7a9be0219da1f9890d6e16f3faa3707af
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2023-12-21 00:11:23 +0700

    Fix
    -Font error
    - Logo centralization

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const Footer = () => {
   /* mt-auto or margin-top will make footer always on the bottom
@@ -8,23 +7,23 @@ const Footer = () => {
     <div className="w-[1512px] h-[215px] relative shrink-0">
       <div className="w-[1512px] h-[215px] left-0 top-0 absolute bg-teal-600"></div>
       <div className="left-[28px] top-[87px] absolute justify-start items-center gap-3 inline-flex">
-        <img className="w-[42px] h-[42px] rounded-[100px]" src="public\johnjud.png" />
-        <div className="text-white text-base font-bold font-['Poppins']">JOHNJUD</div>
+        <img className="w-[42px] h-[42px] rounded-[100px]" src="public\johnjud.png" alt="Johnjud Logo" />
+        <div className="text-white text-base font-bold font-Poppins">JOHNJUD</div>
       </div>
       <div className="left-[208px] top-[37px] absolute flex-col justify-start items-start gap-4 inline-flex">
-        <div className="w-[458px] text-white text-xl font-normal font-['Poppins'] leading-[25px]">สโมสรนิสิตคณะสัตวแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย ถนน อังรีดูนังต์ แขวงปทุมวัน เขตปทุมวัน กรุงเทพ ฯ 10330</div>
-        <div className="w-[397px] text-white text-xl font-normal font-['Poppins'] leading-[25px]">พัฒนาโดย ฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย</div>
+        <div className="w-[458px] text-white text-xl font-normal font-Poppins leading-[25px]">สโมสรนิสิตคณะสัตวแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย ถนน อังรีดูนังต์ แขวงปทุมวัน เขตปทุมวัน กรุงเทพ ฯ 10330</div>
+        <div className="w-[397px] text-white text-xl font-normal font-Poppins leading-[25px]">พัฒนาโดย ฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย</div>
       </div>
 
       <div className="left-[1400px] top-[40px] absolute flex-col justify-start items-center gap-6 inline-flex">
-        <div className="w-[32px] h-[32px] relative">
-        <img className="w-[24px] h-[24px] mx-auto my-auto" src="public\FacebookLogo.svg" />
+        <div className="w-[32px] h-[32px] relative flex items-center justify-center">
+        <img className="w-[24px] h-[24px]" src="public\FacebookLogo.svg" alt="Facebook Logo"/>
         </div>
-        <div className="w-[32px] h-[32px] relative">
-          <img className="w-[24px] h-[24px] mx-auto my-auto" src="public\InstagramLogo.svg" />
+        <div className="w-[32px] h-[32px] relative flex items-center justify-center">
+          <img className="w-[24px] h-[24px]" src="public\InstagramLogo.svg" alt="Instagram Logo"/>
         </div>
-        <div className="w-[32px] h-[32px] relative">
-          <img className="w-[24px] h-[24px] mx-auto my-auto" src="public\TwitterLogo.svg" />
+        <div className="w-[32px] h-[32px] relative flex items-center justify-center">
+          <img className="w-[24px] h-[24px]" src="public\TwitterLogo.svg" alt="Twitter Logo" />
         </div>
       </div>
     </div>
