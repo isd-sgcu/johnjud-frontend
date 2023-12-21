154d0a746a99ad0db5cdb9e7fa703023f53394a4
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2023-12-22 02:28:44 +0700

    Feat: Responsive footer
    Still need to debug code to be compatible with lint

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,34 +5,64 @@ import twitterLogo from '../assets/footer/InstagramLogo.svg';
 import instagramLogo from '../assets/footer/TwitterLogo.svg';
 console.log(facebookLogo);
 const Footer = () => {
-  /* mt-auto or margin-top will make footer always on the bottom
-  please delete this comment after you read it */
+  return (
+    <div className="w-full mt-auto">
+      <div className="w-full h-[243px] xl:h-[215px] relative shrink-0 mx-auto">
 
-  return <div className="mt-auto">
-    <div className="w-[1512px] h-[215px] relative shrink-0">
-      <div className="w-[1512px] h-[215px] left-0 top-0 absolute bg-teal-600"></div>
-      <div className="left-[28px] top-[87px] absolute justify-start items-center gap-3 inline-flex">
-        <img className="w-[42px] h-[42px] rounded-[100px]" src={johnjudLogo} alt="Johnjud Logo" />
-        <div className="text-white text-base font-bold font-Poppins">JOHNJUD</div>
-      </div>
-      <div className="left-[208px] top-[37px] absolute flex-col justify-start items-start gap-4 inline-flex">
-        <div className="w-[458px] text-white text-xl font-normal font-Poppins leading-[25px]">สโมสรนิสิตคณะสัตวแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย ถนน อังรีดูนังต์ แขวงปทุมวัน เขตปทุมวัน กรุงเทพ ฯ 10330</div>
-        <div className="w-[397px] text-white text-xl font-normal font-Poppins leading-[25px]">พัฒนาโดย ฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย</div>
-      </div>
+        <div className="w-full h-full left-0 top-0 absolute bg-teal-600">
 
-      <div className="left-[1400px] top-[40px] absolute flex-col justify-start items-center gap-6 inline-flex">
-        <div className="w-[32px] h-[32px] relative flex items-center justify-center">
-        <img className="w-[24px] h-[24px]" src={facebookLogo} alt="Facebook Logo"/>
-        </div>
-        <div className="w-[32px] h-[32px] relative flex items-center justify-center">
-          <img className="w-[24px] h-[24px]" src={instagramLogo} alt="Instagram Logo"/>
-        </div>
-        <div className="w-[32px] h-[32px] relative flex items-center justify-center">
-          <img className="w-[24px] h-[24px]" src={twitterLogo} alt="Twitter Logo" />
-        </div>
+          <div className="hidden xl:block">
+            <div className="left-[1.85%] top-[40.46%] w-[8.74%] h-[19.53%] absolute
+          md:justify-start md:items-center md:gap-3 inline-flex ">
+              <img className="w-[42px] h-[42px] rounded-full" src={johnjudLogo} alt="Johnjud Logo" />
+              <div className="text-white text-base font-bold font-Poppins">JOHNJUD</div>
+            </div>
+            {/*Text*/}
+            <div className="absolute md:left-[14%] md:top-[17.21%] w-[30.3%] h-[66.6%] flex-col justify-center items-start gap-4 inline-flex">
+              <div className="w-full md:w-full text-white text-xl font-normal font-Poppins leading-[25px]">
+                สโมสรนิสิตคณะสัตวแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย ถนน อังรีดูนังต์ แขวงปทุมวัน เขตปทุมวัน กรุงเทพ ฯ 10330
+              </div>
+              <div className="w-full md:w-[86.69%] text-white text-xl font-normal font-Poppins leading-[25px]">
+                พัฒนาโดย ฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย
+              </div>
+            </div>
+          </div>
+          {/* Below md*/}
+          <div className='xl:hidden left-[7.63%] top-[12.20%] w-[55%] h-[76.27%] flex flex-col justify-start items-start gap-4 absolute'>
+            <div className="left-[0%] top-[0%] w-[62.85%] h-[25.61%] justify-start items-center gap-3 inline-flex">
+              <img className="w-[42px] h-[42px] rounded-full" src={johnjudLogo} alt="Johnjud Logo" />
+              <div className="text-white text-base font-bold font-['Poppins']">JOHNJUD</div>
+            </div>
+            {/*Text*/}
+
+            <div className="text-white text-xs font-normal font-'Poppins' leading-[15px]">
+              สโมสรนิสิตคณะสัตวแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย ถนน อังรีดูนังต์ แขวงปทุมวัน เขตปทุมวัน กรุงเทพ ฯ 10330
+            </div>
+            <div className="text-white text-xs font-normal font-'Poppins' leading-[15px]">
+              พัฒนาโดย ฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย
+            </div>
+          </div>
+          {/*Social*/}
+          <div className="left-[81.42%] top-[20%] xl:left-[92%]  xl:top-[13.6%]  relative flex-col justify-start items-center gap-6 inline-flex">
+            <div className="relative flex items-center justify-center">
+              <img className="w-8 h-8" src={facebookLogo} alt="Facebook Logo" />
+            </div>
+            <div className="relative flex items-center justify-center">
+              <img className="w-8 h-8" src={twitterLogo} alt="Instagram Logo" />
+            </div>
+            <div className="relative flex items-center justify-center">
+              <img className="w-6 h-6" src={instagramLogo} alt="Twitter Logo" />
+            </div>
+          </div>
+        </div >
+
+
       </div>
-    </div>
-  </div>;
+      {/*Only available in less than md */}
+      {/*Johnjud*/}
+
+    </div >
+  );
 };
 
 export default Footer;
