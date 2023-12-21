42fdf2ba4bea280f9bcf87bb5cefab304c68d20f
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2023-12-21 17:34:03 +0700

    fix: change image importing
    as commander boom said

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,9 @@
-
+import React from 'react';
+import johnjudLogo from '../assets/footer/JohnJudLogo.svg';
+import facebookLogo from '../assets/footer/FacebookLogo.svg';
+import twitterLogo from '../assets/footer/InstagramLogo.svg';
+import instagramLogo from '../assets/footer/TwitterLogo.svg';
+console.log(facebookLogo);
 const Footer = () => {
   /* mt-auto or margin-top will make footer always on the bottom
   please delete this comment after you read it */
@@ -7,7 +12,7 @@ const Footer = () => {
     <div className="w-[1512px] h-[215px] relative shrink-0">
       <div className="w-[1512px] h-[215px] left-0 top-0 absolute bg-teal-600"></div>
       <div className="left-[28px] top-[87px] absolute justify-start items-center gap-3 inline-flex">
-        <img className="w-[42px] h-[42px] rounded-[100px]" src="public\johnjud.png" alt="Johnjud Logo" />
+        <img className="w-[42px] h-[42px] rounded-[100px]" src={johnjudLogo} alt="Johnjud Logo" />
         <div className="text-white text-base font-bold font-Poppins">JOHNJUD</div>
       </div>
       <div className="left-[208px] top-[37px] absolute flex-col justify-start items-start gap-4 inline-flex">
@@ -17,13 +22,13 @@ const Footer = () => {
 
       <div className="left-[1400px] top-[40px] absolute flex-col justify-start items-center gap-6 inline-flex">
         <div className="w-[32px] h-[32px] relative flex items-center justify-center">
-        <img className="w-[24px] h-[24px]" src="public\FacebookLogo.svg" alt="Facebook Logo"/>
+        <img className="w-[24px] h-[24px]" src={facebookLogo} alt="Facebook Logo"/>
         </div>
         <div className="w-[32px] h-[32px] relative flex items-center justify-center">
-          <img className="w-[24px] h-[24px]" src="public\InstagramLogo.svg" alt="Instagram Logo"/>
+          <img className="w-[24px] h-[24px]" src={instagramLogo} alt="Instagram Logo"/>
         </div>
         <div className="w-[32px] h-[32px] relative flex items-center justify-center">
-          <img className="w-[24px] h-[24px]" src="public\TwitterLogo.svg" alt="Twitter Logo" />
+          <img className="w-[24px] h-[24px]" src={twitterLogo} alt="Twitter Logo" />
         </div>
       </div>
     </div>
