16f5590553d33bb606f69d91f4166e0997731c17
HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
2023-12-22 01:42:15 +0700

    refactor: using Lint:fix

--- src/app/faq.tsx
+++ src/app/faq.tsx
@@ -1,51 +1,57 @@
 import QuestionBox from "../components/QuestionBox";
 
 const Questions = [
-    {
-        question: 'อยากรับแมวจากชมรมต้องทำอะไรบ้าง ',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lobortis lectus non lectus accumsan, efficitur dapibus tellus aliquam. Aenean imperdiet felis eu purus iaculis, vitae congue velit maximus. Suspendisse ac lorem at velit pretium maximus. Maecenas congue, lorem sed rhoncus pharetra, mi nisi pellentesque enim, vitae faucibus tellus mi mollis augue. Nam a turpis iaculis, dignissim enim ut, tincidunt elit. Sed id pretium leo, ut rhoncus est. Nulla quis magna vel ex venenatis varius in id velit. Donec porttitor justo in scelerisque ultricies.',
-    },
-    {
-        question: 'เลี้ยงแมว 1 ตัวต้องเตรียมอะไรบ้าง',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lobortis lectus non lectus accumsan, efficitur dapibus tellus aliquam. Aenean imperdiet felis eu purus iaculis, vitae congue velit maximus. Suspendisse ac lorem at velit pretium maximus. Maecenas congue, lorem sed rhoncus pharetra, mi nisi pellentesque enim, vitae faucibus tellus mi mollis augue. Nam a turpis iaculis, dignissim enim ut, tincidunt elit. Sed id pretium leo, ut rhoncus est. Nulla quis magna vel ex venenatis varius in id velit. Donec porttitor justo in scelerisque ultricies..',
-    },
-    {
-        question: 'สามารถรับน้องแมวจากชมรมกลับบ้านได้เร็วสุดแค่ไหน',
-        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    },
-    {
-        question: 'พรี่บูมรักแมวมากจนต้องนอกใจหมาได้ไหม',
-        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    }
+  {
+    question: "อยากรับแมวจากชมรมต้องทำอะไรบ้าง ",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lobortis lectus non lectus accumsan, efficitur dapibus tellus aliquam. Aenean imperdiet felis eu purus iaculis, vitae congue velit maximus. Suspendisse ac lorem at velit pretium maximus. Maecenas congue, lorem sed rhoncus pharetra, mi nisi pellentesque enim, vitae faucibus tellus mi mollis augue. Nam a turpis iaculis, dignissim enim ut, tincidunt elit. Sed id pretium leo, ut rhoncus est. Nulla quis magna vel ex venenatis varius in id velit. Donec porttitor justo in scelerisque ultricies.",
+  },
+  {
+    question: "เลี้ยงแมว 1 ตัวต้องเตรียมอะไรบ้าง",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lobortis lectus non lectus accumsan, efficitur dapibus tellus aliquam. Aenean imperdiet felis eu purus iaculis, vitae congue velit maximus. Suspendisse ac lorem at velit pretium maximus. Maecenas congue, lorem sed rhoncus pharetra, mi nisi pellentesque enim, vitae faucibus tellus mi mollis augue. Nam a turpis iaculis, dignissim enim ut, tincidunt elit. Sed id pretium leo, ut rhoncus est. Nulla quis magna vel ex venenatis varius in id velit. Donec porttitor justo in scelerisque ultricies..",
+  },
+  {
+    question: "สามารถรับน้องแมวจากชมรมกลับบ้านได้เร็วสุดแค่ไหน",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  },
+  {
+    question: "พรี่บูมรักแมวมากจนต้องนอกใจหมาได้ไหม",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  },
 ];
 
 const FAQ = () => {
-
-    return <>
-        <div className="relative  text-teal-600 pb-16 px-10 mt-9">
-            <div className="flex flex-col items-center mb-6">
-                <div className="text-4xl font-semibold leading-normal mb-3 items-center">
-                    <p>
-                        คำถามที่พบบ่อย
-                    </p>
-                </div>
-                <div className="relative flex w-full bg-gray-100 rounded-full p-3 shadow">
-                    <img src="../../public/bubble.svg" alt="_blank" />
-                    <input type="text" placeholder="ค้นหาคำถาม" className="bg-transparent rounded-md text-black ml-3 w-full" />{" "}
-                </div>
-            </div>
-            {Questions.map((question) => {
-                return (
-                    <QuestionBox question={question.question} answer={question.answer} />
-                );
-            })}
-
+  return (
+    <>
+      <div className="relative  mt-9 px-10 pb-16 text-teal-600">
+        <div className="mb-6 flex flex-col items-center">
+          <div className="mb-3 items-center text-4xl font-semibold leading-normal">
+            <p>คำถามที่พบบ่อย</p>
+          </div>
+          <div className="relative flex w-full rounded-full bg-gray-100 p-3 shadow">
+            <img src="../../public/bubble.svg" alt="_blank" />
+            <input
+              type="text"
+              placeholder="ค้นหาคำถาม"
+              className="ml-3 w-full rounded-md bg-transparent text-black"
+            />{" "}
+          </div>
         </div>
-       
-
-            
+        {Questions.map((question) => {
+          return (
+            <QuestionBox
+              key={question.question}
+              question={question.question}
+              answer={question.answer}
+            />
+          );
+        })}
+      </div>
     </>
-
+  );
 };
 
 export default FAQ;
--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,31 +1,47 @@
 import { useState } from "react";
 
-const questionBox = ({question, answer}: { question: string, answer: string }) => {
+const questionBox = ({
+  question,
+  answer,
+}: {
+  question: string;
+  answer: string;
+}) => {
+  const [showed, setShowed] = useState(false);
 
-    const [showed, setShowed] = useState(false);
-
-    return (
-    <div className="bg-white rounded-3xl flex flex-col shadow-lg mb-6">
-        <div
-            className="flex flex-row my-3 rounded-3xl px-8 py-4 items-center justify-between "
-            onClick={() => setShowed(!showed)}
-        >
-            <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                    <img className="w-6 h-6 mr-3" src="../../public/question_icon.svg" alt="_blank" />
-                    <h2 className="text-xl font-medium leading-normal">    
-                        {question}
-                    </h2>
-                </div>
-            </div>
-            <img className={"w-4 h-2 transition-all duration-300 " + (showed ? "rotate-180" : "")} src="../../public/back_icon.svg" alt="_blank" />
+  return (
+    <div className="mb-6 flex flex-col rounded-3xl bg-white shadow-lg">
+      <div
+        className="my-3 flex flex-row items-center justify-between rounded-3xl px-8 py-4 "
+        onClick={() => setShowed(!showed)}
+      >
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <img
+              className="mr-3 h-6 w-6"
+              src="../../public/question_icon.svg"
+              alt="_blank"
+            />
+            <h2 className="text-xl font-medium leading-normal">{question}</h2>
+          </div>
         </div>
-        <div className={"transition-all duration-300 " + (showed ? "block" : "hidden")}>      
-                <p className="mx-6 mb-6 font-light text-gray-500 text-lg">
-                    {answer}
-                </p>
-            </div>
+        <img
+          className={
+            "h-2 w-4 transition-all duration-300 " +
+            (showed ? "rotate-180" : "")
+          }
+          src="../../public/back_icon.svg"
+          alt="_blank"
+        />
+      </div>
+      <div
+        className={
+          "transition-all duration-300 " + (showed ? "block" : "hidden")
+        }
+      >
+        <p className="mx-6 mb-6 text-lg font-light text-gray-500">{answer}</p>
+      </div>
     </div>
-    );
+  );
 };
 export default questionBox;
