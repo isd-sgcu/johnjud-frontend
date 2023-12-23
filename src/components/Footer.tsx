e7615384d98c9cbd82bd89376caae5e63324cd2a
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2023-12-23 22:03:13 +0700

    Refactor: using "rem" instead of "px"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,20 +11,18 @@ const Footer = () => {
           <div className="flex-start m-0 inline-flex w-[66%] flex-col justify-start gap-4 lg:w-[45%] lg:flex-row lg:items-center lg:gap-0">
             <div className="mr-12 inline-flex items-center justify-start gap-3">
               <img
-                className="h-[42px] w-[42px] rounded-full"
-                src={johnjudLogo}
-                alt="Johnjud Logo"
+                className="h-11 w-11 rounded-full" src={johnjudLogo} alt="Johnjud Logo"
               />
               <div className="text-sm font-bold text-white">JOHNJUD</div>
             </div>
             {/*Text*/}
-            <div className="m-0 inline-flex flex-col gap-4 p-0 lg:w-[458px] lg:flex-wrap lg:gap-3">
-              <div className="w-full text-xs font-normal text-white lg:text-base">
+            <div className="m-0 inline-flex flex-col gap-4 p-0 lg:w-114rem lg:flex-wrap lg:gap-3">
+              <div className="w-full text-xs font-normal text-white lg:text-base lg:w-[90%]">
                 สโมสรนิสิตคณะสัตวแพทยศาสตร์ <br className="gap-3"></br>
                 จุฬาลงกรณ์มหาวิทยาลัย ถนน อังรีดูนังต์ แขวงปทุมวัน เขตปทุมวัน
                 กรุงเทพ ฯ 10330
               </div>
-              <div className="w-full text-xs font-normal text-white lg:w-[90%] lg:text-base">
+              <div className="w-full text-xs font-normal text-white lg:w-[80%] lg:text-base">
                 พัฒนาโดย ฝ่ายพัฒนาระบบสารสนเทศ <br className="gap-3"></br>
                 องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย
               </div>
