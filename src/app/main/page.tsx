59cd1f4afdf03aa10bed04bb6a8a03947dfa024b
K.Chanatipz <chanatipzaza2003@gmail.com>
2023-12-25 23:26:22 +0700

    fix: KUYBOOM

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,31 +1,43 @@
-import johnjudLogo from "../../assets/main/johnjudLogo.svg";
 import catImage from "../../assets/main/catImage.svg";
+import johnjudLogo from "../../assets/main/johnjudLogo.svg";
 
 const Main = () => {
-    return (
-        <div className="bg-primary w-full shrink-0 pt-7 xl:pt-16">
-            <div className="xl:flex xl:flex-row-reverse xl:gap-5 xl:justify-center">
-                <img className="h-52 w-52 mx-auto xl:mx-0" src={johnjudLogo} alt="Johnjud Logo" />
-                <div className="uppercase text-white text-center mt-1.5 
-                xl:flex xl:flex-col xl:content-end">
-                    <p className="text-4xl font-bold py-2.5 xl:flex xl:items-center
-                    xl:text-[156px] xl:py-0 xl:flex-1 xl:align-middle">
-                        johnjud
-                    </p>
-                    <p className="text-base font-medium py-1 xl:text-2xl xl:ml-96">
-                        Best Option - Adoption
-                    </p>
-                </div>
-            </div>
-            <div className="flex justify-center items-center align-middle">
-                <button className="position absolute center cursor-pointer border border-solid border-secondary rounded-2xl 
-                bg-secondary px-32 py-2.5 opacity-80
-                text-white text-2xl font-semibold
-                mt-80 mb-20 hover:ease-in">กดดูเลย</button>
-                <img src={catImage} alt="Cat Image" className="mx-auto" />
-            </div>
+  return (
+    <div className="w-full shrink-0 bg-primary pt-7 xl:pt-16">
+      <div className="xl:flex xl:flex-row-reverse xl:justify-center xl:gap-5">
+        <img
+          className="mx-auto h-52 w-52 xl:mx-0"
+          src={johnjudLogo}
+          alt="Johnjud Logo"
+        />
+        <div
+          className="mt-1.5 text-center uppercase text-white 
+                xl:flex xl:flex-col xl:content-end"
+        >
+          <p
+            className="py-2.5 text-4xl font-bold xl:flex xl:flex-1
+                    xl:items-center xl:py-0 xl:align-middle xl:text-[156px]"
+          >
+            johnjud
+          </p>
+          <p className="py-1 text-base font-medium xl:ml-96 xl:text-2xl">
+            Best Option - Adoption
+          </p>
         </div>
-    );
+      </div>
+      <div className="flex items-center justify-center align-middle">
+        <button
+          className="position center absolute mb-20 mt-80 cursor-pointer rounded-2xl border 
+                border-solid border-secondary bg-secondary px-32
+                py-2.5 text-2xl font-semibold
+                text-white opacity-80 hover:ease-in"
+        >
+          กดดูเลย
+        </button>
+        <img src={catImage} alt="Cat Image" className="mx-auto" />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
