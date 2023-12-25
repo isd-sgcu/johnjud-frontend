f0c16a0df6e28b9ea85e8a32c359237344530f69
K.Chanatipz <chanatipzaza2003@gmail.com>
2023-12-25 23:23:13 +0700

    fix: pc ratio

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -3,21 +3,22 @@ import catImage from "../../assets/main/catImage.svg";
 
 const Main = () => {
     return (
-        <div className="bg-primary mt-auto w-full shrink-0">
-            <div className="xl:flex xl:flex-row-reverse">
-                <img className="h-52 w-52 mx-auto" src={johnjudLogo} alt="Johnjud Logo" />
+        <div className="bg-primary w-full shrink-0 pt-7 xl:pt-16">
+            <div className="xl:flex xl:flex-row-reverse xl:gap-5 xl:justify-center">
+                <img className="h-52 w-52 mx-auto xl:mx-0" src={johnjudLogo} alt="Johnjud Logo" />
                 <div className="uppercase text-white text-center mt-1.5 
-                xl:flex xl:flex-col xl:content-end xl:mt-16 xl:ml-56">
-                    <div className="text-4xl font-bold py-2.5 xl:text-[156px]">
+                xl:flex xl:flex-col xl:content-end">
+                    <p className="text-4xl font-bold py-2.5 xl:flex xl:items-center
+                    xl:text-[156px] xl:py-0 xl:flex-1 xl:align-middle">
                         johnjud
-                    </div>
-                    <div className="text-base font-medium py-1 xl:text-2xl xl:ml-96">
+                    </p>
+                    <p className="text-base font-medium py-1 xl:text-2xl xl:ml-96">
                         Best Option - Adoption
-                    </div>
+                    </p>
                 </div>
             </div>
             <div className="flex justify-center items-center align-middle">
-                <button className="position absolute center border border-solid border-secondary rounded-2xl 
+                <button className="position absolute center cursor-pointer border border-solid border-secondary rounded-2xl 
                 bg-secondary px-32 py-2.5 opacity-80
                 text-white text-2xl font-semibold
                 mt-80 mb-20 hover:ease-in">กดดูเลย</button>
