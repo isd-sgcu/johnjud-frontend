112f8bd6b515303243c05bde4e848844a501d4ed
K.Chanatipz <chanatipzaza2003@gmail.com>
2023-12-25 22:10:58 +0700

    fix: red button

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -7,19 +7,21 @@ const Main = () => {
             <div className="flex justify-center">
                 <img className="h-52 w-52" src={johnjudLogo} alt="Johnjud Logo" />
             </div>
-            <div className="mt-1.5">
-                <div className="uppercase text-white text-center text-4xl font-bold py-2.5">
+            <div className="mt-1.5 xl:flex xl:flex-col xl:content-end">
+                <div className="uppercase text-white text-center text-4xl font-bold py-2.5
+                xl:text-[156px]">
                     johnjud
                 </div>
-                <div className="uppercase text-white text-center text-base font-medium py-1">
+                <div className="uppercase text-white text-center text-base font-medium py-1
+                xl:text-2xl xl:ml-96">
                     Best Option - Adoption
                 </div>
             </div>
-            <div>
-                <button className="position absolute border border-solid border-secondary rounded-2xl 
+            <div className="flex justify-center items-center align-middle">
+                <button className="position absolute center border border-solid border-secondary rounded-2xl 
                 bg-secondary px-32 py-2.5 opacity-80
                 text-white text-2xl font-semibold
-                mt-80 mb-20">กดดูเลย</button>
+                mt-80 mb-20 hover:ease-in">กดดูเลย</button>
                 <img src={catImage} alt="Cat Image" className="mx-auto" />
             </div>
         </div>
