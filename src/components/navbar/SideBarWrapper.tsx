1583797101fe45c53c2d68cc6a0eee2d0ee73206
teegoood <teegooodgood@gmail.com>
2023-12-27 23:03:41 +0700

    refactor: pnpm lint:fix

--- src/components/navbar/SideBarWrapper.tsx
+++ src/components/navbar/SideBarWrapper.tsx
@@ -7,28 +7,32 @@ interface SideBarWrapperProps {
   toggle: boolean;
 }
 
-const SideBarWrapper: React.FC<SideBarWrapperProps> = ({ color, setToggle, toggle }) => {
-  const [z , setZ] = useState('-z-10');
-  const opacity  = toggle ? 'opacity-1' : 'opacity-0';
+const SideBarWrapper: React.FC<SideBarWrapperProps> = ({
+  color,
+  setToggle,
+  toggle,
+}) => {
+  const [z, setZ] = useState("-z-10");
+  const opacity = toggle ? "opacity-1" : "opacity-0";
   const transitionDuration = 700;
   const delay = 500;
-  
+
   useEffect(() => {
-    if(toggle){
-      setZ('z-10');
-    }
-    else{
-      setTimeout(() => setZ('-z-10'), transitionDuration - delay);
+    if (toggle) {
+      setZ("z-10");
+    } else {
+      setTimeout(() => setZ("-z-10"), transitionDuration - delay);
     }
   }, [toggle]);
-  
 
   function handleBackgroundClick() {
     setToggle(false);
   }
 
   return (
-    <div className={`fixed bottom-0 left-0 right-0 top-0 flex ${opacity} ${z} transition duration-${transitionDuration}`}>
+    <div
+      className={`fixed bottom-0 left-0 right-0 top-0 flex ${opacity} ${z} transition duration-${transitionDuration}`}
+    >
       <div className="h-full flex-1" onClick={handleBackgroundClick}></div>
       <SideBar color={color} />
     </div>
