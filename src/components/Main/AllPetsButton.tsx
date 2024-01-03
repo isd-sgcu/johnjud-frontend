b460fea5d6fe42caad7a575512eb73c98bdf9797
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-03 16:32:08 +0700

    refactor: run lint

--- src/components/Main/AllPetsButton.tsx
+++ src/components/Main/AllPetsButton.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const AllPetsButton = () => {
   return (
     <Link
-        className="flex items-center justify-center gap-2.5 rounded-full bg-primary px-6 py-2 font-bold text-white shadow"
-        to = "/pets"
-        >
+      className="flex items-center justify-center gap-2.5 rounded-full bg-primary px-6 py-2 font-bold text-white shadow"
+      to="/pets"
+    >
       ดูสัตว์เลี้ยงท้งหมด
     </Link>
   );
