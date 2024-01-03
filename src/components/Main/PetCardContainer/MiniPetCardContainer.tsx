4203b967d0e909c44dfad8ac4a3c3a7067189b0c
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-03 16:28:54 +0700

    feat: add banner

--- src/components/Main/PetCardContainer/MiniPetCardContainer.tsx
+++ src/components/Main/PetCardContainer/MiniPetCardContainer.tsx
@@ -2,9 +2,9 @@ import mockPetData from "../../../mock/mockPetData";
 import MiniPetCard from "../../card/MiniPetCard";
 
 const MiniPetCardContainer = () => {
-  const pets = mockPetData;
+  const pets = mockPetData.slice(0,8);
   return (
-    <div className="grid grid-cols-2 gap-x-1 gap-y-2">
+    <div className="grid grid-cols-2 gap-x-1 gap-y-2 md:gap-6 md:grid-cols-4">
       {pets.map((pet, index) => (
         <MiniPetCard
           key={index}
