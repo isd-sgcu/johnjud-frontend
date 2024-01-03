3ca256ff6b06be58835a771cd55c0548e086a33f
Zenith <sqoulo11@gmail.com>
2024-01-03 22:15:27 +0700

    feat: responsive pc in user-create page

--- src/app/admin/user-create/page.tsx
+++ src/app/admin/user-create/page.tsx
@@ -4,12 +4,12 @@ import AddThumpnail from "../../../components/admin/user-create/AddThumpnail";
 import EditName from "../../../components/admin/user-create/EditName";
 import EditText from "../../../components/admin/user-create/EditText";
 import MainLayout from "../../../layouts/MainLayout";
-import EditInfo from "../../../components/admin/user-create/EditInfo";
+import EditInfoAndSubmit from "../../../components/admin/user-create/EditInfoAndSubmit";
 import AddSmallPicture from "../../../components/admin/user-create/AddSmallPicture";
-import PetBadge from "../../../components/admin/user-create/PetBadge";
+import johnjudLogo from "../../../assets/johnjud-with-text.png"
 
 const userCreate = () => {
-  const [name, setName] = useState("...กรุณาใส่ชื่อ");
+  const [name, setName] = useState("กรุณาใส่ชื่อ...");
   const [text, setText] = useState("");
   const [thumpnail, setThumpnail] = useState();
   const [origin, setOrigin] = useState("fromClub");
@@ -24,30 +24,45 @@ const userCreate = () => {
     sterile: false
   });
 
-  return (
-    <div className="flex w-screen flex-col pb-28 pt-6 lg:pb-20 lg:pt-8">
-      {/* TODO: responsive */}
-      <div className="flex items-center justify-between overflow-clip px-[10%] py-2">
-        <Icon icon="ion:chevron-back" color="#08878E" className="flex flex-none w-8 h-8" />
-        <EditName value={name} setValue={setName} />
-      </div>
-      <div className="mx-[10%] flex h-1 rounded-full w-1/5 self-end bg-primary" />
+  const handleSubmit = () => {
 
-      <div className="flex flex-col px-6 pt-8 items-center">
-        <AddThumpnail valueThumpnail={thumpnail} setThumpnail={setThumpnail} valueOrigin={origin} setOrigin={setOrigin}/>
+  }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 w-screen flex-col pb-28 pt-6 lg:pb-20 lg:pt-8">
+      {/* Icon return & small EditName */}
+      <div className="flex items-center lg:col-span-3 justify-between overflow-clip px-[10%] lg:px-12 py-2">
+        <Icon icon="ion:chevron-back" color="#08878E" className="flex flex-none w-8 h-8" />
+        <div className="flex flex-col w-full visible lg:hidden">
+          <EditName value={name} setValue={setName} />
+        </div>
+      </div>
+
+      {/* Thumpnail */}
+      <div className="flex w-full justify-center px-6 pt-8 lg:pl-[25%] lg:pr-0 lg:justify-start">
+        <AddThumpnail valueThumpnail={thumpnail} setThumpnail={setThumpnail} valueOrigin={origin} setOrigin={setOrigin} />
+      </div>
+
+      {/* large EditName + EditText */}
+      <div className="flex flex-col w-full px-6 pt-8 lg:col-span-2 lg:justify-between lg:pr-[12.5%]">
+        <div className="w-full hidden lg:flex lg:flex-col lg:mb-4">
+          <EditName value={name} setValue={setName} />
+        </div>
         <EditText value={text} setValue={setText} />
       </div>
-      <div className="flex w-full pl-6">
+
+      {/* Small Picture */}
+      <div className="flex w-full pl-6 lg:col-span-3 lg:pl-[8.33%]">
         <AddSmallPicture value={pictures} setValue={setPictures} />
       </div>
-      <div className="flex flex-col mt-8 lg:mt-10">
-        <EditInfo value={info} setValue={setInfo} />
+
+      {/* Edit info + Pose Button*/}
+      <div className="flex flex-col mt-8 lg:mt-10 lg:col-span-2 lg:h-full">
+        <EditInfoAndSubmit value={info} setValue={setInfo} onSubmit={handleSubmit}/>
       </div>
-      
-      <div className="flex px-[10%] w-full mt-6 cursor-pointer">
-        <div className="py-2 rounded-3xl bg-[#C81425] text-center text-white font-semibold text-2xl w-full">
-          โพสต์เลย
-        </div>
+
+      <div className="hidden lg:flex w-full justify-center object-contain lg:mt-10">
+        <img src={johnjudLogo} className="flex w-44 h-64"></img>
       </div>
     </div>
   );
--- src/components/admin/user-create/EditName.tsx
+++ src/components/admin/user-create/EditName.tsx
@@ -26,28 +26,34 @@ const EditName = (props) => {
     }
   }, [enableEdit]);
   return (
-    <div className="flex w-full">
+    <div className="flex flex-col w-full relative">
       <div
-        className={`flex w-full flex-row items-center justify-end ${
+        className={`flex w-fit flex-row items-center self-end lg:self-start ${
           enableEdit ? "hidden" : "visible"
         }`}
       >
-        <Icon icon="custom:pencil" color="#C81425" className="flex flex-none w-6 h-6 cursor-pointer" onClick={handleClickEdit} />
-        <span className="pl-1 flex text-wrap break-all text-right text-3xl font-bold text-primary">
+        <Icon icon="custom:pencil" color="#C81425" className="flex flex-none w-6 h-6 cursor-pointer lg:hidden" onClick={handleClickEdit} />
+        <span className="pl-1 flex text-wrap break-all text-right lg:text-left text-3xl font-bold text-primary">
           {props.value}
         </span>
+        <Icon icon="custom:pencil" color="#C81425" className="hidden flex-none w-6 h-6 cursor-pointer lg:flex lg:ml-3" onClick={handleClickEdit} />
       </div>
+
       <TextareaAutosize
         value={props.value}
         onChange={handleOnChange}
-        className={`flex w-full resize-none text-wrap break-all rounded-lg p-2 text-right text-3xl font-bold text-primary focus:outline-[#D9D9D9] ${
+        className={`flex w-full resize-none text-wrap break-all rounded-lg p-2 text-right lg:text-left text-3xl font-bold text-primary focus:outline-[#D9D9D9] ${
           enableEdit ? "visible" : "hidden"
         }`}
         disabled={!enableEdit}
         onBlur={handleOnBlur}
         ref={ref}
         rows={1}
+        placeholder="กรุณาใส่ชื่อ..."
       />
+
+      
+      <div className="flex w-full mt-1 pt-1 rounded-full max-w-20 lg:max-w-32 self-end lg:self-start bg-primary" />
     </div>
   );
 };
