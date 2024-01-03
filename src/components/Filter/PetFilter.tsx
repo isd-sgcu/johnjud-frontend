13439d19d2ba0d3cea9f373d25061df57ccda463
boomchanotai <boom9387@hotmail.com>
2024-01-03 12:06:21 +0700

    feat: add icon

--- src/components/Filter/PetFilter.tsx
+++ src/components/Filter/PetFilter.tsx
@@ -1,7 +1,8 @@
+import { Icon } from "@iconify/react";
 const PetFilter = () => {
   return (
-    <div className="h-14 w-14 rounded-full bg-accent-red">
-      <div></div>
+    <div className="flex h-14 w-14 cursor-pointer flex-col items-center justify-center rounded-full bg-accent-red hover:brightness-90">
+      <Icon icon="@custom:md:cat-white" className="h-6 w-6" />
     </div>
   );
 };
--- src/components/Search/PetSearch.tsx
+++ src/components/Search/PetSearch.tsx
@@ -3,7 +3,9 @@ import { Icon } from "@iconify/react";
 const PetSearch = () => {
   return (
     <div className="flex w-full flex-row items-center justify-start gap-3 rounded-full bg-white px-6 py-3 shadow">
-      <Icon icon="mdi:magnify" className="h-6 w-6 text-accent-red" />
+      <div>
+        <Icon icon="@custom:md:bubble-red" className="h-6 w-6" />
+      </div>
       <input
         type="text"
         placeholder="ค้นหาสัตว์เลี้ยง"
