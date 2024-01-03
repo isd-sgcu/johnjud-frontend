06e19ea45bfb5613e6548cc9c3af7b45b10c61b1
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-03 17:03:40 +0700

    fix: lint fix

--- src/components/Main/AllPetsButton.tsx
+++ src/components/Main/AllPetsButton.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const AllPetsButton = () => {
   return (
     <Link
-      className="flex items-center w-full md:w-auto justify-center gap-2.5 rounded-full bg-primary px-6 py-2 font-bold text-white shadow"
+      className="flex w-full items-center justify-center gap-2.5 rounded-full bg-primary px-6 py-2 font-bold text-white shadow md:w-auto"
       to="/pets"
     >
       ดูสัตว์เลี้ยงท้งหมด
--- src/components/Main/Divider.tsx
+++ src/components/Main/Divider.tsx
@@ -2,7 +2,7 @@ import pawIcon from "../../assets/main/paw.svg";
 
 const DividerMobile = () => {
   return (
-    <div className="hidden md:flex items-center justify-center">
+    <div className="hidden items-center justify-center md:flex">
       <img src={pawIcon} alt="pawIcon" className="h-9 w-9" />
     </div>
   );
