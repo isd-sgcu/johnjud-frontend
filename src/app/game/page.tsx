d4856d80fa9b042f6b26453de8f147e8a538acaf
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-01-04 11:13:14 +0700

    feat: add moblie ratio

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,16 +2,16 @@ import catImage from "../../assets/user-tarot/ANS 4 แมวดำ 1.svg";
 
 const userTarotFirstpage = () => {
     return (
-        <div className="min-h-screen w-full shrink-0 bg-primary-variant py-10
+        <div className="min-h-screen w-full shrink-0 bg-primary-variant py-6
         flex flex-col items-center">
             <img className="w-80 h-[428px] rounded-2xl"
                 src={catImage} />
-            <div className="flex flex-col items-center font-bold py-8">
-                <div className="text-primary text-4xl py-2">เกมทายนิสัย</div>
-                <div className="text-white text-xl py-1.5">มาหาสัตว์เลี้ยงที่เหมาะกับคุณกัน!</div></div>
-            <div w-10 h-10 bg-red></div>
+            <div className="flex flex-col items-center font-bold my-6">
+                <div className="text-primary text-5xl py-3">เกมทายนิสัย</div>
+                <div className="text-white text-base">มาหาสัตว์เลี้ยงที่เหมาะกับคุณกัน!</div>
+            </div>
             <button className="text-white text-xl font-semibold text-center
-            bg-secondary px-6 py-2 w-80 rounded-2xl">เล่นเลย</button>
+            bg-secondary px-6 py-2 rounded-2xl self-stretch mx-6">เล่นเลย</button>
         </div >
     );
 };
