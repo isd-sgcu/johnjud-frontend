0f354367fb6e6ade1584d8f9acfc160401cdbce7
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-01-04 16:27:23 +0700

    feat: add pc responsive

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,14 +2,20 @@ import catImage from "../../assets/user-tarot/ANS 4 แมวดำ 1.svg";
 
 const userTarotFirstpage = () => {
     return (
-        <div className="bg-primary-variant w-full min-h-screen p-6 flex flex-col items-center">
-            <img className="w-80 h-[428px] rounded-2xl" src={catImage} />
-            <div className="flex flex-col items-center my-6">
-                <div className="text-primary text-5xl font-bold py-3">เกมทายนิสัย</div>
-                <div className="text-white text-base font-bold">มาหาสัตว์เลี้ยงที่เหมาะกับคุณกัน!</div>
-            </div>
-            <button className="text-white text-xl font-semibold text-center w-full
+        <div className="bg-primary-variant w-full min-h-screen">
+            <div className="p-6 flex flex-col items-center lg:py-10 lg:w-80 lg:mx-auto lg:px-0">
+                <img className="w-80 h-[428px] rounded-2xl"
+                    src={catImage}
+                />
+                <div className="flex flex-col items-center my-6 lg:my-8">
+                    <div className="text-primary text-5xl font-bold py-3
+                    lg:text-4xl lg:py-2">เกมทายนิสัย</div>
+                    <div className="text-white text-base font-bold
+                    lg:text-xl lg:py-1">มาหาสัตว์เลี้ยงที่เหมาะกับคุณกัน!</div>
+                </div>
+                <button className="text-white text-xl font-semibold text-center w-full
                 bg-secondary px-6 py-2 rounded-2xl">เล่นเลย</button>
+            </div>
             {/* <div className="w-90 py-6 flex flex-col items-center lg:py-10">
                 <img className="w-full h-[428px] rounded-2xl"
                     src={catImage} />
