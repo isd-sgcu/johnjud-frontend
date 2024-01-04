8f2a2b5a28751e0f213c62b4e5a4846b20cee90b
boomchanotai <boom9387@hotmail.com>
2024-01-04 17:41:14 +0700

    feat: add admin pet page

---
+++ src/app/admin/pets/page.tsx
@@ -0,0 +1,53 @@
+import Button from "@/components/Button";
+import Container from "@/components/Container";
+import Filter from "@/components/Filter";
+import { PetIcon } from "@/components/Filter/Icon";
+import Heading from "@/components/Pets/Heading";
+import PetSearch from "@/components/Search/PetSearch";
+import MainLayout from "@/layouts/MainLayout";
+
+import { useCallback, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Pets = () => {
+  const [isOpenFilterPanel, setIsOpenFilterPanel] = useState(false);
+
+  const toggleIsOpenFilterPanel = useCallback(() => {
+    setIsOpenFilterPanel((prev) => !prev);
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Heading />
+      </Container>
+      <Container className="flex flex-col items-center space-y-6">
+        <div className="flex w-full flex-row gap-x-4">
+          <PetSearch />
+          <div className="relative">
+            <PetIcon
+              isOpen={isOpenFilterPanel}
+              onClick={toggleIsOpenFilterPanel}
+            />
+            <Filter isOpen={isOpenFilterPanel} />
+          </div>
+        </div>
+        <Link to="/admin/pets/add">
+          <Button
+            icon="ph:plus"
+            text="สร้างโพสต์ใหม่"
+            variant="accent-red"
+            rounded="full"
+            className="px-24"
+          />
+        </Link>
+      </Container>
+      <Container>
+        <div>{/* Pet Card */}</div>
+      </Container>
+    </>
+  );
+};
+
+export const Layout = MainLayout;
+export default Pets;
