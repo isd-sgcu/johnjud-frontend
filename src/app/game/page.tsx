6e979e3dcbf7bb6dedbddcd286251fb20efd1427
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-01-04 14:53:04 +0700

    fix: fix containing div

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,18 +2,28 @@ import catImage from "../../assets/user-tarot/ANS 4 แมวดำ 1.svg";
 
 const userTarotFirstpage = () => {
     return (
-        <div className="min-h-screen w-full shrink-0 bg-primary-variant py-6
-        flex flex-col items-center">
-            <img className="w-80 h-[428px] rounded-2xl"
-                src={catImage} />
-            <div className="flex flex-col items-center font-bold my-6">
-                <div className="text-primary text-5xl py-3">เกมทายนิสัย</div>
-                <div className="text-white text-base">มาหาสัตว์เลี้ยงที่เหมาะกับคุณกัน!</div>
+        <div className="bg-primary-variant w-full min-h-screen p-6 flex flex-col items-center">
+            <img className="w-80 h-[428px] rounded-2xl" src={catImage} />
+            <div className="flex flex-col items-center my-6">
+                <div className="text-primary text-5xl font-bold py-3">เกมทายนิสัย</div>
+                <div className="text-white text-base font-bold">มาหาสัตว์เลี้ยงที่เหมาะกับคุณกัน!</div>
             </div>
-            <button className="text-white text-xl font-semibold text-center
-            bg-secondary px-6 py-2 rounded-2xl self-stretch mx-6">เล่นเลย</button>
-        </div >
+            <button className="text-white text-xl font-semibold text-center w-full
+                bg-secondary px-6 py-2 rounded-2xl">เล่นเลย</button>
+            {/* <div className="w-90 py-6 flex flex-col items-center lg:py-10">
+                <img className="w-full h-[428px] rounded-2xl"
+                    src={catImage} />
+                <div className="flex flex-col items-center font-bold my-6 lg:my-8">
+                    <div className="text-primary text-5xl py-3 lg:text-4xl lg:py-4">เกมทายนิสัย</div>
+                    <div className="text-white text-base lg:text-xl lg:py-1">มาหาสัตว์เลี้ยงที่เหมาะกับคุณกัน!</div>
+                </div>
+                <button className="text-white text-xl font-semibold text-center w-full bg-secondary px-6 py-2 rounded-2xl">เล่นเลย</button>
+            </div > */}
+        </div>
     );
 };
+
+// min-h-screen w-full shrink-0 -> biggest div
+// self-stretch mx-6 lg:w-80 lg:mx-auto -> btn
 
 export default userTarotFirstpage;
