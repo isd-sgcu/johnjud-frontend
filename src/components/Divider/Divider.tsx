2caaf26ab22c860ee0bbb19e3d5c95561ba5e06a
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-05 20:31:44 +0700

    refactor: Merge 'divider and 'mobile divider' into a single responsive component

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -2,8 +2,8 @@ import { Icon } from "@iconify/react";
 
 const Divider = () => {
   return (
-    <div className="hidden items-center justify-center md:flex">
-      <Icon icon="ph:paw-print" className="h-9 w-9 text-primary" />
+    <div className="flex items-center justify-center ">
+      <Icon icon="ph:paw-print" className="h-6 w-6 text-primary md:h-9 md:w-9" />
     </div>
   );
 };
