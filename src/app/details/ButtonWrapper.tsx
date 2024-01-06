c0e7b7247b68799db2e49341489b7d219064dc0a
teegoood <teegooodgood@gmail.com>
2024-01-06 13:08:08 +0700

    refactor: change color name

--- src/app/details/ButtonWrapper.tsx
+++ src/app/details/ButtonWrapper.tsx
@@ -5,11 +5,11 @@ const ButtonWrapper = ({ isAdmin }: { isAdmin: boolean }) => {
     <div className="flex flex-col gap-2.5 font-semibold text-white xl:justify-between">
       <div className="justify-between xl:flex">
         <div className="flex gap-4 xl:flex-col xl:items-start xl:gap-4">
-          <div className="flex items-center justify-center gap-2 rounded-full bg-secondary px-3 py-1 shadow-[0_0_2px_1px] shadow-zinc-300">
+          <div className="flex items-center justify-center gap-2 rounded-full bg-accent-red px-3 py-1 shadow-[0_0_2px_1px] shadow-zinc-300">
             <Icon icon="ph:eyedropper-bold" color="white" className="h-4 w-4" />
             <span>ฉีดวัคซีนแล้ว</span>
           </div>
-          <div className="justify-centerpy-1 flex items-center gap-2 rounded-full bg-secondary px-3 py-1 shadow-[0_0_2px_1px] shadow-zinc-300">
+          <div className="justify-centerpy-1 flex items-center gap-2 rounded-full bg-accent-red px-3 py-1 shadow-[0_0_2px_1px] shadow-zinc-300">
             <Icon icon="ph:medal" color="white" className="h-4 w-4" />
             <span>ทําหมันแล้ว</span>
           </div>
@@ -27,7 +27,7 @@ const ButtonWrapper = ({ isAdmin }: { isAdmin: boolean }) => {
           <div className="flex items-center rounded-2xl bg-primary px-6 py-2 shadow-[0_0_2px_1px] shadow-zinc-300">
             ถูกรับเลี้ยงแล้ว
           </div>
-          <button className="flex items-center gap-2 rounded-2xl bg-secondary px-6 py-2 text-lg font-normal shadow-[0_0_2px_1px] shadow-zinc-300">
+          <button className="flex items-center gap-2 rounded-2xl bg-accent-red px-6 py-2 text-lg font-normal shadow-[0_0_2px_1px] shadow-zinc-300">
             <Icon icon="lucide:trash-2" color="white" />
             <span>ลบโพสต์</span>
           </button>
