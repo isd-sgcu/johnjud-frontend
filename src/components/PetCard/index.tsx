6a0414b41d81a5af5e584640ff9b30cfcf12cb20
ChanatpakornD <csirintronsopon8@gmail.com>
2024-01-06 23:10:04 +0700

    refactor: code redundant

--- src/components/PetCard/index.tsx
+++ src/components/PetCard/index.tsx
@@ -2,7 +2,6 @@ import Button from "@/components/Button";
 import PetDetail from "@/components/PetCard/PetDetail";
 import TogglePetButton from "@/components/PetCard/TogglePetButton";
 import { Icon } from "@iconify/react";
-import { useState } from "react";
 import Johnjud from "../../assets/main/johnjudLogo.svg";
 
 export enum PetStatus {
@@ -16,7 +15,8 @@ export enum PetGender {
 }
 
 type PetCardProps = {
-  role: string;
+  role: "user" | "admin";
+  image: string;
   type: string;
   name: string;
   status: PetStatus;
@@ -31,6 +31,7 @@ type PetCardProps = {
 
 const PetCard = ({
   role = "admin",
+  image = Johnjud,
   type = "dog",
   name = "ไรลีย์, พันธุ์แลบราดดอร์",
   status = PetStatus.adopted,
@@ -39,7 +40,7 @@ const PetCard = ({
   habit = "นิสัยขี้เล่น",
   isSterile = true,
   isLiked = false,
-  isVisibled = true,
+  isVisibled = false,
   onClick,
 }: PetCardProps) => {
   const likedHeart = isLiked ? "ph:heart-fill" : "ph:heart";
@@ -47,23 +48,20 @@ const PetCard = ({
   const petSterile = isSterile ? "ทำหมันแล้ว" : "ยังไม่ได้ทำหมัน";
   const adoptedButton =
     status === PetStatus.adopted ? "disabled" : "accent-red";
-  const [isVisible, setIsVisible] = useState(isVisibled);
-  if (role === "user" && isVisibled === true) {
+  if (role === "user" && isVisibled === false) {
+    return;
+  } else {
     return (
       <div
         className="flex w-80 flex-col items-start justify-start rounded-2xl bg-white p-4 shadow"
         onClick={() => onClick}
       >
-        <img
-          src={Johnjud}
-          alt={type}
-          className="mb-4 w-72 rounded-2xl shadow"
-        />
+        <img src={image} alt={type} className="mb-4 w-72 rounded-2xl shadow" />
         <div className="mb-2 flex w-72 flex-row items-center justify-between">
           <p className="text-2xl font-bold text-black">{name}</p>
           <button onClick={() => {}}>
             <Icon
-              icon={likedHeart}
+              icon={role === "admin" ? "ph:pencil-simple" : likedHeart}
               className="relative h-8 w-8 text-accent-red"
             />
           </button>
@@ -77,48 +75,16 @@ const PetCard = ({
             <PetDetail icon={"ph:music-notes"} description={habit} />
             <PetDetail icon={"ph:medal"} description={petSterile} />
           </div>
-          <Button
-            text={"รับเลี้ยง"}
-            variant={adoptedButton}
-            rounded="full"
-            className="max-h-10 max-w-28 text-base"
-          />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        className="flex w-80 flex-col items-start justify-start rounded-2xl bg-white p-4 shadow"
-        onClick={() => onClick}
-      >
-        <img
-          src={Johnjud}
-          alt="_blank"
-          className="mb-4 w-72 rounded-2xl shadow"
-        />
-        <div className="mb-2 flex w-72 flex-row items-center justify-between">
-          <p className="text-2xl font-bold text-black">{name}</p>
-          <button onClick={() => {}} className="text-accent-red cursor-pointer">
-            <Icon
-              icon={"ph:pencil-simple"}
-              className="relative h-9 w-9"
+          {role === "admin" ? (
+            <TogglePetButton visibility={isVisibled} />
+          ) : (
+            <Button
+              text={"รับเลี้ยง"}
+              variant={adoptedButton}
+              rounded="full"
+              className="max-h-10 max-w-28 text-base"
             />
-          </button>
-        </div>
-        <div className="flex w-72 flex-row items-end justify-between">
-          <div className=" items-center">
-            <PetDetail
-              icon={"ph:paw-print"}
-              description={`${petGender}, ${age}`}
-            />
-            <PetDetail icon={"ph:music-notes"} description={habit} />
-            <PetDetail icon={"ph:medal"} description={petSterile} />
-          </div>
-          <TogglePetButton
-            visibility={isVisible}
-            onChange={()=>{}}
-          />
+          )}
         </div>
       </div>
     );
