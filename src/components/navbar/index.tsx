2cbc1a7a2305a878e61e53b3d7b976c5e2c8f2c9
teegoood <teegooodgood@gmail.com>
2024-01-06 14:05:06 +0700

    refactor: lint:fix

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,64 +1,74 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import logoPrimary from "../../assets/Navbar/logoPrimary.svg";
 import logoSecondary from "../../assets/Navbar/logoSecondary.svg";
 import SideBarWrapper from "./SideBarWrapper";
-import { Icon } from "@iconify/react/dist/iconify.js";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const pathname = useLocation().pathname;
   const [style, setStyle] = useState({
-    color : "bg-primary",
-    logo : logoPrimary
-  })
+    color: "bg-primary",
+    logo: logoPrimary,
+  });
 
   useEffect(() => {
     switch (pathname) {
       case "/admin":
         setStyle({
-          color : "bg-accent-red",
-          logo : logoPrimary
+          color: "bg-accent-red",
+          logo: logoPrimary,
         });
         break;
       case "/horo":
         setStyle({
-          color : "bg-accent-purple",
-          logo : logoSecondary
+          color: "bg-accent-purple",
+          logo: logoSecondary,
         });
         break;
       case "/no-logo":
         setStyle({
           color: "bg-primary",
-          logo : ""
+          logo: "",
         });
         break;
-      default :
+      default:
         setStyle({
           color: "bg-primary",
-          logo : logoPrimary
+          logo: logoPrimary,
         });
         break;
     }
   }, [pathname]);
 
   return (
-    <div className={`flex items-center justify-between px-4 py-3 transition-all xl:px-8 xl:py-6 ${style.color}`}>
+    <div
+      className={`flex items-center justify-between px-4 py-3 transition-all xl:px-8 xl:py-6 ${style.color}`}
+    >
       <div className="h-18 flex items-center gap-3 ">
         {style.logo && (
-          <img src={style.logo} alt="logo" className="h-12 w-12 xl:h-16 xl:w-16" />
+          <img
+            src={style.logo}
+            alt="logo"
+            className="h-12 w-12 xl:h-16 xl:w-16"
+          />
         )}
         <div className="cursor-pointer text-lg font-bold text-white xl:text-2xl">
           JOHNJUD
         </div>
       </div>
-      <Icon 
-        icon="radix-icons:hamburger-menu" 
-        color="white" 
-        className="h-8 w-8 xl:h-12 xl:w-12 cursor-pointer"
+      <Icon
+        icon="radix-icons:hamburger-menu"
+        color="white"
+        className="h-8 w-8 cursor-pointer xl:h-12 xl:w-12"
         onClick={() => setToggle(true)}
       />
-      <SideBarWrapper setToggle={setToggle} color={style.color} toggle={toggle} />
+      <SideBarWrapper
+        setToggle={setToggle}
+        color={style.color}
+        toggle={toggle}
+      />
     </div>
   );
 };
