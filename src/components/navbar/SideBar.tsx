1db0dbd56048952499fee1ef6c9708139589e26f
teegoood <teegooodgood@gmail.com>
2024-01-07 14:55:02 +0700

    refactor: lint:fix

--- src/components/navbar/SideBar.tsx
+++ src/components/navbar/SideBar.tsx
@@ -1,6 +1,6 @@
+import blankProfile from "@/assets/Navbar/blankProfile.svg";
 import { Icon } from "@iconify/react";
 import React from "react";
-import blankProfile from "@/assets/Navbar/blankProfile.svg";
 import { Link } from "react-router-dom";
 
 interface SideBarProps {
@@ -24,31 +24,52 @@ const SideBar: React.FC<SideBarProps> = ({ color }) => {
       </div>
       <div>
         <div className="flex flex-col justify-start gap-6">
-          <Link to="/home" className="flex cursor-pointer items-center gap-1.5 xl:gap-4">
+          <Link
+            to="/home"
+            className="flex cursor-pointer items-center gap-1.5 xl:gap-4"
+          >
             <Icon icon="ph:house" color="white" className="h-6 w-6" />
             <span>หน้าหลัก</span>
           </Link>
-          <Link to="/pets" className="flex cursor-pointer items-center gap-1.5 xl:gap-4">
+          <Link
+            to="/pets"
+            className="flex cursor-pointer items-center gap-1.5 xl:gap-4"
+          >
             <Icon icon="ph:horse" color="white" className="h-6 w-6" />
             <span>สัตว์เลี้ยง</span>
           </Link>
-          <Link to="/game" className="flex cursor-pointer items-center gap-1.5 xl:gap-4">
+          <Link
+            to="/game"
+            className="flex cursor-pointer items-center gap-1.5 xl:gap-4"
+          >
             <Icon icon="ph:game-controller" color="white" className="h-6 w-6" />
             <span>เล่นเกม</span>
           </Link>
-          <Link to="/horo" className="flex cursor-pointer items-center gap-1.5 xl:gap-4">
+          <Link
+            to="/horo"
+            className="flex cursor-pointer items-center gap-1.5 xl:gap-4"
+          >
             <Icon icon="ph:moon-stars" color="white" className="h-6 w-6" />
             <span>ดูดวง</span>
           </Link>
-          <Link to="/frequently-asked-questions" className="flex cursor-pointer items-center gap-1.5 xl:gap-4">
+          <Link
+            to="/frequently-asked-questions"
+            className="flex cursor-pointer items-center gap-1.5 xl:gap-4"
+          >
             <Icon icon="ph:question" color="white" className="h-6 w-6" />
             <span>คำถามที่พบบ่อย</span>
           </Link>
-          <Link to="/about" className="flex cursor-pointer items-center gap-1.5 xl:gap-4">
+          <Link
+            to="/about"
+            className="flex cursor-pointer items-center gap-1.5 xl:gap-4"
+          >
             <Icon icon="ph:info" color="white" className="h-6 w-6" />
             <span>เกี่ยวกับเรา</span>
           </Link>
-          <Link to="/admin" className="flex cursor-pointer items-center gap-1.5 xl:gap-4">
+          <Link
+            to="/admin"
+            className="flex cursor-pointer items-center gap-1.5 xl:gap-4"
+          >
             <Icon icon="ph:lock-key" color="white" className="h-6 w-6" />
             <span>Admin Login</span>
           </Link>
