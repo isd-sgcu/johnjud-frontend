b8caf59f81207d0a6205eb4ade2a06b9a89da956
teegoood <teegooodgood@gmail.com>
2024-01-07 14:46:03 +0700

    refactor: refactor index.tsx style

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
     <div
       className={`flex items-center justify-between px-4 py-3 transition-all xl:px-8 xl:py-6 ${style.color}`}
     >
-      <div className="flex h-18 items-center gap-3 ">
+      <div className="flex items-center gap-3 ">
         {style.logo && (
           <img
             src={style.logo}
@@ -54,14 +54,13 @@ const Navbar = () => {
             className="h-12 w-12 xl:h-16 xl:w-16"
           />
         )}
-        <div className="cursor-pointer text-lg font-bold text-white xl:text-2xl">
+        <div className="text-lg font-bold text-white xl:text-2xl">
           JOHNJUD
         </div>
       </div>
       <Icon
         icon="radix-icons:hamburger-menu"
-        color="white"
-        className="h-8 w-8 cursor-pointer xl:h-12 xl:w-12"
+        className="h-8 w-8 cursor-pointer xl:h-12 xl:w-12 text-white"
         onClick={() => setToggle(true)}
       />
       <SideBarWrapper
