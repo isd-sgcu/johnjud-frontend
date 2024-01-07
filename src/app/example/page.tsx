f561cf9b530a08dfd1a7038c8c50f1e3c51235e5
ChanatpakornD <csirintronsopon8@gmail.com>
2024-01-07 21:23:08 +0700

    feat: useMemo

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,10 +1,11 @@
+import PetCard from "@/components/PetCard";
 import MainLayout from "@/layouts/MainLayout";
-
 // Page
 const ExamplePage = () => {
   return (
     <>
       <h2 className="text-2xl font-bold">ExamplePage</h2>
+      <PetCard />
     </>
   );
 };
--- src/components/PetCard/index.tsx
+++ src/components/PetCard/index.tsx
@@ -1,7 +1,10 @@
 import Button from "@/components/Button";
 import PetDetail from "@/components/PetCard/PetDetail";
 import TogglePetButton from "@/components/PetCard/TogglePetButton";
+import { UtcStringToYearMonth } from "@/utils/dateConverter";
 import { Icon } from "@iconify/react";
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
 import Johnjud from "../../assets/main/johnjudLogo.svg";
 
 export enum PetStatus {
@@ -9,85 +12,100 @@ export enum PetStatus {
   adopted,
 }
 
-export enum PetGender {
-  male,
-  female,
-}
-
 type PetCardProps = {
   role: "user" | "admin";
+  id: string;
   image: string;
   type: string;
   name: string;
   status: PetStatus;
-  gender: PetGender;
-  age: string;
+  gender: "male" | "female";
+  birthDate: string;
   habit: string;
   isSterile: boolean;
   isLiked: boolean;
   isVisibled: boolean;
-  onClick?: () => void;
 };
 
 const PetCard = ({
-  role = "admin",
+  role,
+  id,
   image = Johnjud,
-  type = "dog",
-  name = "ไรลีย์, พันธุ์แลบราดดอร์",
-  status = PetStatus.adopted,
-  gender = PetGender.female,
-  age = "อายุ 6 ปี 11 เดือน",
-  habit = "นิสัยขี้เล่น",
-  isSterile = true,
-  isLiked = false,
-  isVisibled = false,
-  onClick,
+  name,
+  status,
+  gender,
+  birthDate,
+  habit,
+  isSterile,
+  isLiked,
+  isVisibled,
 }: PetCardProps) => {
-  const likedHeart = isLiked ? "ph:heart-fill" : "ph:heart";
-  const petGender = gender === PetGender.female ? "ตัวเมีย" : "ตัวผู้";
-  const petBirthDate = 
-  const petSterile = isSterile ? "ทำหมันแล้ว" : "ยังไม่ได้ทำหมัน";
-  const adoptedButton =
-    status === PetStatus.adopted ? "disabled" : "accent-red";
+  const likedHeart = useMemo(() => {
+    return isLiked ? "ph:heart-fill" : "ph:heart";
+  }, [isLiked]);
+
+  const linkTo = useMemo(() => {
+    return "/pets/" + id;
+  }, [id]);
+
+  const { years, months } = useMemo(() => {
+    return UtcStringToYearMonth({ utcString: birthDate });
+  }, [birthDate]);
+
+  const petGender = useMemo(() => {
+    return gender === "female" ? "ตัวเมีย" : "ตัวผู้";
+  }, [gender]);
+
+  const petSterile = useMemo(() => {
+    return isSterile ? "ทำหมันแล้ว" : "ยังไม่ได้ทำหมัน";
+  }, [isSterile]);
+
+  const adoptedButton = useMemo(() => {
+    return status === PetStatus.adopted ? "disabled" : "accent-red";
+  }, [status]);
+
   if (role === "user" && isVisibled === false) {
     return;
   } else {
     return (
-      <div
-        className="flex w-80 flex-col items-start justify-start rounded-2xl bg-white p-4 shadow"
-        onClick={() => onClick}
-      >
-        <img src={image} alt={type} className="mb-4 w-72 rounded-2xl shadow" />
-        <div className="mb-2 flex w-72 flex-row items-center justify-between">
-          <p className="text-2xl font-bold text-black">{name}</p>
-          <button onClick={() => {}}>
-            <Icon
-              icon={role === "admin" ? "ph:pencil-simple" : likedHeart}
-              className="relative h-8 w-8 text-accent-red"
-            />
-          </button>
-        </div>
-        <div className="flex w-72 flex-row items-end justify-between">
-          <div className=" items-center">
-            <PetDetail
-              icon={"ph:paw-print"}
-              description={`${petGender}, ${age}`}
-            />
-            <PetDetail icon={"ph:music-notes"} description={habit} />
-            <PetDetail icon={"ph:medal"} description={petSterile} />
+      <Link to={linkTo}>
+        <div className="flex w-80 flex-col items-start justify-start rounded-2xl bg-white p-4 shadow">
+          <img
+            src={image}
+            alt={name}
+            className="mb-4 w-72 rounded-2xl shadow"
+          />
+          <div className="mb-2 flex w-72 flex-row items-center justify-between">
+            <p className="text-2xl font-bold text-black">{name}</p>
+            <button onClick={() => {}}>
+              <Icon
+                icon={role === "admin" ? "ph:pencil-simple" : likedHeart}
+                className="relative h-8 w-8 text-accent-red"
+              />
+            </button>
           </div>
-          {role === "admin" ? (
-            <TogglePetButton visibility={isVisibled} />
-          ) : (
-            <Button
-              text={"รับเลี้ยง"}
-              variant={adoptedButton}
-              rounded="full"
-              className="max-h-10 max-w-28 text-base"
-            />
-          )}
+          <div className="flex w-72 flex-row items-end justify-between">
+            <div className=" items-center">
+              <PetDetail
+                icon={"ph:paw-print"}
+                description={`${petGender}, อายุ ${years} ปี ${months} เดือน`}
+              />
+              <PetDetail icon={"ph:music-notes"} description={habit} />
+              <PetDetail icon={"ph:medal"} description={petSterile} />
+            </div>
+            {role === "admin" ? (
+              <TogglePetButton visibility={isVisibled} />
+            ) : (
+              <Button
+                text={"รับเลี้ยง"}
+                variant={adoptedButton}
+                rounded="full"
+                className="max-h-10 max-w-28 text-base"
+              />
+            )}
+          </div>
         </div>
-      </div>
+      </Link>
     );
   }
 };
