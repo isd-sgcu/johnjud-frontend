53c4ccabac86cd56e39753b0aeb6d259a9cdd449
teegoood <teegooodgood@gmail.com>
2024-01-07 18:50:07 +0700

    refactor: refactor responsive nav stye

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -34,20 +34,20 @@ const Navbar = () => {
   }, [pathname]);
 
   return (
-    <div className={`flex items-center justify-between px-4 py-3 transition-all xl:px-8 xl:py-6 ${style.color}`}>
+    <div className={`flex items-center justify-between px-4 py-3 transition-all  ${style.color}`}>
       <div className="flex items-center gap-3 ">
         {style.logo && (
           <img
             src={style.logo}
             alt="logo"
-            className="h-12 w-12 xl:h-16 xl:w-16"
+            className="h-12 w-12 "
           />
         )}
-        <div className="text-lg font-bold text-white xl:text-2xl">JOHNJUD</div>
+        <div className="text-lg font-bold text-white -xl:text-2xl">JOHNJUD</div>
       </div>
       <Icon
         icon="radix-icons:hamburger-menu"
-        className="h-8 w-8 cursor-pointer text-white xl:h-12 xl:w-12"
+        className="h-8 w-8 cursor-pointer text-white"
         onClick={() => setToggle(true)}
       />
       <SideBarWrapper
