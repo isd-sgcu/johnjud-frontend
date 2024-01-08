133a62fed61e839cabf2a715b7f6a06962311f27
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-01-08 23:26:24 +0700

    fix:fix div

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,7 +4,7 @@ import catImage from "../../assets/user-tarot/catImage.svg";
 
 const userTarotFirstpage = () => {
   return (
-    <div className="w-full bg-primary-variant">
+    <div className="min-h-screen w-full bg-primary-variant">
       <div className="flex flex-col items-center p-6 lg:mx-auto lg:w-80 lg:px-0 lg:py-10">
         <img className="w-80 rounded-2xl" src={catImage} />
         <div className="my-6 flex flex-col items-center lg:my-8">
