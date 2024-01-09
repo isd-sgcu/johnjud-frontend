ed4da3a056314c0f9cc953b452223d9b06189503
teegoood <teegooodgood@gmail.com>
2024-01-09 16:28:23 +0700

    refactor: refactor file path

--- src/components/Pets/Details.tsx
+++ src/components/Pets/Details.tsx
@@ -3,17 +3,19 @@ import BigPetCard from "@/components/Details/BigPetCard";
 import Info from "@/components/Details/Info";
 import MainLayout from "@/layouts/MainLayout";
 
-const Details = () => {
-  const isAdmin = true;
-
+const Details = ({
+    isAdmin
+} : {
+    isAdmin :boolean
+}) => {
   return (
-    <div className="pt-4">
+    <>
       <BigPetCard isAdmin={isAdmin} />
       <div className="my-8 flex gap-20 xl:justify-between xl:pr-24">
         <Info isAdmin={isAdmin} />
         <img src={logo} alt="logo" className="hidden h-64 w-64 xl:block" />
       </div>
-    </div>
+    </>
   );
 };
 
