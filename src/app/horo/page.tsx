0192034077f449974a634bee09c37a9ed65d93eb
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-09 20:18:03 +0700

    feat: add 9 cards assets for horo games

--- src/app/horo/page.tsx
+++ src/app/horo/page.tsx
@@ -1,23 +1,25 @@
+import SelectCard from "@/components/Card/SelectCard";
 import Container from "@/components/Container";
+import Divider from "@/components/Divider";
 import HoroBgContainer from "@/components/Horo/HoroBgContainer";
 import MainLayout from "@/layouts/MainLayout";
-import SelectCard from "@/components/Card/SelectCard";
-import Divider from "@/components/Divider";
 import { Icon } from "@iconify/react"; // Import the Icon component correctly
 
 const HoroPage = () => {
   return (
     <>
-      <Container className="flex justify-center items-center">
+      <Container className="flex items-center justify-center">
         <HoroBgContainer>
-          <div className="flex md:hidden justify-center items-center space-x-3">
+          <div className="flex items-center justify-center space-x-3 md:hidden">
             <Icon icon="custom:md:line" />
             <Divider variant="accent-yellow" />
-            <Icon icon="custom:md:line" className=""/>
+            <Icon icon="custom:md:line" />
           </div>
-          <h2 className="text-2xl text-white font-bold text-nowrap">เลือกไพ่ 1 ใบเพื่อทำนาย</h2>
+          <h2 className="text-nowrap text-2xl font-bold text-white">
+            เลือกไพ่ 1 ใบเพื่อทำนาย
+          </h2>
           <SelectCard />
-          <div className="flex md:hidden justify-center items-center space-x-3">
+          <div className="flex items-center justify-center space-x-3 md:hidden">
             <Icon icon="custom:md:line" />
             <Divider variant="accent-yellow" />
             <Icon icon="custom:md:line" />
--- src/components/Card/HoroCard.tsx
+++ src/components/Card/HoroCard.tsx
@@ -1,12 +1,11 @@
 import HoroCardBack from "@/assets/horo/card-cover.png";
 
 const HoroCard = () => {
-    return (
-      <div className="cursor-pointer shadow-purple-500 hover:shadow-2xl transition-all duration-500 hover:scale-110 rounded-[10px] shrink-0">
-          <img src={HoroCardBack} alt="HoroCard" className="w-20 rounded-[10px]" />
-      </div>
-    )
-}
-  
+  return (
+    <div className="shrink-0 cursor-pointer rounded-[10px] shadow-purple-500 transition-all duration-100 hover:scale-110">
+      <img src={HoroCardBack} alt="HoroCard" className="w-20 rounded-[10px]" />
+    </div>
+  );
+};
+
 export default HoroCard;
-  
