b1aa0baad33a972a702fee3b2d767716b89d9fd8
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-09 15:48:38 +0700

    feat: add SelectCard

---
+++ src/components/Card/SelectCard.tsx
@@ -0,0 +1,20 @@
+import HoroCard from "@/components/Card/HoroCard";
+
+const SelectCard = () => {
+
+  return (
+    <div className="grid grid-cols-3 gap-3">
+        <HoroCard />
+        <HoroCard />
+        <HoroCard />
+        <HoroCard />
+        <HoroCard />
+        <HoroCard />
+        <HoroCard />
+        <HoroCard />
+        <HoroCard />
+    </div>
+  )
+}
+
+export default SelectCard
