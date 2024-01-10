5d1fe25bd86cb58d1941ad15ce1bdcbedcc783af
ChanatpakornD <csirintronsopon8@gmail.com>
2024-01-10 20:29:27 +0700

    refactor: pnpm lint

--- src/components/PetThumbnails/index.tsx
+++ src/components/PetThumbnails/index.tsx
@@ -6,7 +6,7 @@ interface PetThumbnailsProps {
 
 const PetThumbnails = ({ petLink }: PetThumbnailsProps) => {
   return (
-    <div className="relative aspect-square min-w-40 sm:min-w-80 rounded-[20px]">
+    <div className="relative aspect-square min-w-40 rounded-[20px] sm:min-w-80">
       <div className="absolute -top-1 right-4">
         {/* Recieved where they're from */}
         <PetBadge value={"fromHome"} setValue={() => {}} isEditabled={false} />
@@ -14,7 +14,7 @@ const PetThumbnails = ({ petLink }: PetThumbnailsProps) => {
       <img
         src={petLink}
         alt="pets"
-        className="aspect-square min-w-40 sm:min-w-80 overflow-hidden rounded-2xl object-cover object-center shadow"
+        className="aspect-square min-w-40 overflow-hidden rounded-2xl object-cover object-center shadow sm:min-w-80"
       />
     </div>
   );
