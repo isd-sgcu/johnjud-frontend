513d5a17bc673f4dffa453d642dce967a4833bb9
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2024-01-10 11:53:28 +0700

    Fix: fixing based on senior

--- src/hooks/usePathStyle.ts
+++ src/hooks/usePathStyle.ts
@@ -1,7 +1,8 @@
 import { useMemo } from "react";
 import { useLocation } from "react-router-dom";
-import logoPrimary from "../assets/footer/logoPrimary.svg";
-import logoSecondary from "../assets/footer/logoSecondary.svg";
+import logoPrimary from "../assets/navbar/logoPrimary.svg";
+import logoSecondary from "../assets/navbar/logoSecondary.svg";
+
 const usePathStyle = () => {
   const pathname = useLocation().pathname;
 
@@ -15,11 +16,6 @@ const usePathStyle = () => {
       return {
         color: "bg-accent-purple",
         logo: logoSecondary,
-      };
-    } else if (pathname.includes("/no-logo")) {
-      return {
-        color: "bg-primary",
-        logo: "",
       };
     } else {
       return {
--- src/utils/iconify.ts
+++ src/utils/iconify.ts
@@ -172,7 +172,7 @@ addIcon("custom:md:dog-white", {
   width: 30,
   height: 30,
 });
-addIcon("custom:md:twitter-logo", {
+addIcon("custom:md:x-logo", {
   body: `
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 <path d="M22.0414 2L5.16339 22H1.84631L18.6268 2H22.0414Z" fill="white"/>
