36c11508b75fb43c7e31dbb7d9ee78b450277e92
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-11 22:01:53 +0700

    fix: 'Divider.tsx' variant not working

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -6,11 +6,15 @@ interface DividerProps {
 }
 
 const Divider: React.FC<DividerProps> = ({ variant }) => {
+  const getClassName = () => {
+    return variant === 'primary' ? 'text-primary' : 'text-accent-yellow';
+  };
+
   return (
-    <div className="flex items-center justify-center ">
+    <div className="flex items-center justify-center">
       <Icon
         icon="ph:paw-print"
-        className={`h-6 w-6 text-${variant} md:h-9 md:w-9`}
+        className={`h-6 w-6 ${getClassName()} md:h-9 md:w-9`}
       />
     </div>
   );
