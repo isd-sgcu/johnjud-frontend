e85a95ee3486d8281f3c17df7ddefa839002e57e
Zenith <sqoulo11@gmail.com>
2024-01-11 20:06:07 +0700

    chore: pnpm lint:fix

--- src/app/game/page3/page.tsx
+++ src/app/game/page3/page.tsx
@@ -1,25 +1,26 @@
+import ResultGame from "@/components/Game/ResultGame";
 import GameLayout from "@/layouts/GameLayout";
 import OrangeCat from "../../../assets/game/OrangeCat.jpg";
-import ResultGame from "@/components/Game/ResultGame";
 
 const GamePage3 = () => {
-    const header = "คุณคือแม้ว";
-    const image = OrangeCat;
-    const infoHeader = "แม้วส้ม";
-    const infoText = "คุณเป็นคนที่มีภาวะความเป็นผู้นำและมีความ เชื่อมั่นในตัวเองสูงเป็นคนที่มีเพื่อนเยอะมากๆ แม้ภายนอกจะดูเฟรนลี่ แต่ก็แอบมีกำแพง นิดนึงเพื่อป้องกันตัวเองไม่ให้ตัวเองผิดหวัง จึงมีนิสัยเหมือนแมวส้ม เพราะแมวสี เหล่านี้ส่วนใหญ่จะเป็นแมวที่มีความเป็นมิตร ขี้อ้อน ชอบเป็นหัวหน้าแก๊ง มีความเป็นตัวของ ตัวเองสูง ทำให้ไม่ชอบที่จะตามใจใคร แต่จะชอบตามใจตัวเอง อยากมาหาก็จะมา หาเองแล้วแต่อารมณ์";
-    const handleOnClickSave = () => {};
-    const handleOnClickShare = () => {};
-    return (
-        <ResultGame 
-            header={header}
-            image={image}
-            infoHeader={infoHeader}
-            infoText={infoText}
-            handleOnClickSave={handleOnClickSave}
-            handleOnClickShare={handleOnClickShare}
-        />
-    );
-}
+  const header = "คุณคือแม้ว";
+  const image = OrangeCat;
+  const infoHeader = "แม้วส้ม";
+  const infoText =
+    "คุณเป็นคนที่มีภาวะความเป็นผู้นำและมีความ เชื่อมั่นในตัวเองสูงเป็นคนที่มีเพื่อนเยอะมากๆ แม้ภายนอกจะดูเฟรนลี่ แต่ก็แอบมีกำแพง นิดนึงเพื่อป้องกันตัวเองไม่ให้ตัวเองผิดหวัง จึงมีนิสัยเหมือนแมวส้ม เพราะแมวสี เหล่านี้ส่วนใหญ่จะเป็นแมวที่มีความเป็นมิตร ขี้อ้อน ชอบเป็นหัวหน้าแก๊ง มีความเป็นตัวของ ตัวเองสูง ทำให้ไม่ชอบที่จะตามใจใคร แต่จะชอบตามใจตัวเอง อยากมาหาก็จะมา หาเองแล้วแต่อารมณ์";
+  const handleOnClickSave = () => {};
+  const handleOnClickShare = () => {};
+  return (
+    <ResultGame
+      header={header}
+      image={image}
+      infoHeader={infoHeader}
+      infoText={infoText}
+      handleOnClickSave={handleOnClickSave}
+      handleOnClickShare={handleOnClickShare}
+    />
+  );
+};
 
 export default GamePage3;
 
--- src/components/Game/ResultGame.tsx
+++ src/components/Game/ResultGame.tsx
@@ -1,59 +1,61 @@
 import Button from "../Button";
 
 interface ResultGameProps {
-    header: string,
-    image: string,
-    infoHeader: string,
-    infoText: string,
-    handleOnClickSave: () => void;
-    handleOnClickShare: () => void;
+  header: string;
+  image: string;
+  infoHeader: string;
+  infoText: string;
+  handleOnClickSave: () => void;
+  handleOnClickShare: () => void;
 }
 
 const ResultGame = (props: ResultGameProps) => {
-    return (
-        <div className="flex gap-4 md:gap-6 w-full py-6 md:py-10 px-6 mx-auto justify-center">
-            <div className="flex flex-col w-full max-w-80 gap-4 md:gap-6">
-                {/* Header + Image */}
-                <div className="flex flex-col items-center gap-3 md:gap-4">
-                    <div className="text-primary font-bold md:text-2xl">
-                        {props.header}
-                    </div>
-                    <div className="w-full aspect-[4:3] overflow-hidden rounded-2xl shadow-md">
-                        <img src={props.image} className="w-full h-full object-center object-contain" />
-                    </div>
-                </div>
-
-                {/* Info */}
-                <div className="flex flex-col items-center gap-3 md:gap-4">
-                    <div className="w-full bg-accent-light-gray text-center rounded-2xl px-6 py-2 text-primary font-bold text-2xl">
-                        {props.infoHeader}
-                    </div>
-                    <div className="w-full break-words text-center text-lg">
-                        {props.infoText}
-                    </div>
-                </div>
-
-                {/* Save and Share Button */}
-                <div className="flex flex-row items-center justify-between flex-wrap gap-4">
-                    <Button
-                        text="บันทึกลงเครื่อง"
-                        icon="ic:baseline-save-alt"
-                        onClick={props.handleOnClickSave}
-                        rounded="2xl"
-                        variant="primary"
-                    />
-                    <Button
-                        text="แชร์"
-                        icon="ph:share"
-                        onClick={props.handleOnClickShare}
-                        rounded="2xl"
-                        variant="accent-red"
-                    />
-                </div>
-            </div>
-
+  return (
+    <div className="mx-auto flex w-full justify-center gap-4 px-6 py-6 md:gap-6 md:py-10">
+      <div className="flex w-full max-w-80 flex-col gap-4 md:gap-6">
+        {/* Header + Image */}
+        <div className="flex flex-col items-center gap-3 md:gap-4">
+          <div className="font-bold text-primary md:text-2xl">
+            {props.header}
+          </div>
+          <div className="aspect-[4:3] w-full overflow-hidden rounded-2xl shadow-md">
+            <img
+              src={props.image}
+              className="h-full w-full object-contain object-center"
+            />
+          </div>
         </div>
-    );
-}
+
+        {/* Info */}
+        <div className="flex flex-col items-center gap-3 md:gap-4">
+          <div className="w-full rounded-2xl bg-accent-light-gray px-6 py-2 text-center text-2xl font-bold text-primary">
+            {props.infoHeader}
+          </div>
+          <div className="w-full break-words text-center text-lg">
+            {props.infoText}
+          </div>
+        </div>
+
+        {/* Save and Share Button */}
+        <div className="flex flex-row flex-wrap items-center justify-between gap-4">
+          <Button
+            text="บันทึกลงเครื่อง"
+            icon="ic:baseline-save-alt"
+            onClick={props.handleOnClickSave}
+            rounded="2xl"
+            variant="primary"
+          />
+          <Button
+            text="แชร์"
+            icon="ph:share"
+            onClick={props.handleOnClickShare}
+            rounded="2xl"
+            variant="accent-red"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ResultGame;
