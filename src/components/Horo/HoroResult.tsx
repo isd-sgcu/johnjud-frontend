5f4fa677ef308249fa2fec8ea7155ce5654c78d4
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-11 10:27:30 +0700

    refactor: change width

--- src/components/Horo/HoroResult.tsx
+++ src/components/Horo/HoroResult.tsx
@@ -1,97 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Button from "@/components/Button";
+import ResultDisplay from "@/components/Horo/ResultDisplay";
 
 interface HoroResultProps {
   resultImage: string;
   resultName: string;
-  resultText: string; 
+  resultText: string;
 }
 
-const HoroResult: React.FC<HoroResultProps> = ({ resultImage, resultName,  resultText }) => {
-  /*  
-      state for image result
-        - phase 0 = image
-        - phase 1 = image flipped
-        - phase 2 = text in the image
-  */
+const HoroResult: React.FC<HoroResultProps> = ({ resultImage, resultName, resultText }) => {
   const [phase, setPhase] = useState(0);
   const [showResult, setShowResult] = useState(false);
   
-  // for swapping between phase
   useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>;
-
+    let intervalId : number;
     if (!showResult) {
-      const updatePhase = (currentPhase: number): void => {
-        timer = setTimeout(() => {
-          const nextPhase = (currentPhase + 1) % 3;
-          setPhase(nextPhase);
-          updatePhase(nextPhase);
-        }, 1500);
-      };
-      updatePhase(phase);
+      intervalId = setInterval(() => {
+        setPhase(prevPhase => (prevPhase + 1) % 3);
+      }, 1500);
     }
-    return () => clearTimeout(timer);
-  }, [showResult, phase]); 
 
-  // when we click the phase back to 0 and show the result below
+    return () => clearInterval(intervalId);
+  }, [showResult]);
+
   const handleClick = () => {
     setPhase(0);
     setShowResult(true);
   };
-  
-  const imageContainerClass = "relative w-80 h-[533px]";
-  const overlayClass = 
-  "absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center text-center p-4 rounded-[10px] flex-col space-y-4";
-  
-  let imageClass = "";
-  if (phase === 1) {
-      imageClass += " transform scale-x-[-1]";
-  }
 
+  const imageClass = useMemo(() => {
+    return phase === 1 ? "transform scale-x-[-1]" : "";
+  }, [phase]);
+  
+  const overlayClass = useMemo(() => {
+    return phase === 2 ? "absolute inset-0 bg-black bg-opacity-50" : "";
+  }, [phase]);
+  
   return (
-    <div className={`flex ${showResult ? "md:space-x-8 flex-col md:flex-row" : "flex-col"} items-center justify-center space-y-8`}>
-      <div className={imageContainerClass}>
+    <div className={`flex ${showResult ? "md:flex-row md:space-x-8" : "flex-col"} items-center justify-center space-y-8`}>
+      <div className="relative w-80 h-[533px]">
         <img src={resultImage} alt="ResultImage" className={imageClass} />
-        {phase === 2 && ( 
-          <>
-            <div className={overlayClass}></div>
-            <div className={`${overlayClass} text-white font-bold `}>
-              <h2 className="text-3xl">{resultName}</h2>
-              <p className="text-base">{resultText}</p>
-            </div>
-          </>
+        {overlayClass && (
+          <div className={`${overlayClass} flex justify-center items-center text-center p-4 rounded-[10px] flex-col space-y-4 font-bold text-white`}>
+            <h2 className="text-3xl">{resultName}</h2>
+            <p className="text-base">{resultText}</p>
+          </div>
         )}
       </div>
       <Button
         text="ต่อไป"
         variant="accent-yellow"
         rounded="2xl"
-        className={`${showResult ?  "hidden" : "flex"}  justify-center items-center w-8/12 px-[84px] py-[15px] text-xl font-bold text-accent-purple shadow-inner md:w-auto`}
+        className={`${showResult ? "hidden" : "flex"} w-8/12 px-20 py-4 text-xl font-bold text-accent-purple shadow-inner md:w-auto`}
         onClick={handleClick}
       />
       {showResult && (
-      <div className="flex flex-col space-y-6 items-center text-center w-[290px]">
-          <h2 className="text-3xl font-bold text-black">{resultName}</h2>
-          <p className="font-light text-xl text-black">{resultText}</p>
-          <div className="flex justify-center items-center space-x-3">
-              <Button
-                text="บันทึกลงเครื่อง"
-                variant="accent-purple"
-                className="text-xl font-white font-medium p-2.5 rounded-xl"
-                icon="ph:download-simple"
-              />
-              <Button
-                text="แชร์"
-                variant="accent-yellow"
-                className="text-xl text-black font-medium p-2.5 rounded-xl"
-                icon="ph:share"
-              />
-          </div>
-      </div>
+        <ResultDisplay resultName={resultName} resultText={resultText} />
       )}
     </div>
   );
 };
+
 
 export default HoroResult;
