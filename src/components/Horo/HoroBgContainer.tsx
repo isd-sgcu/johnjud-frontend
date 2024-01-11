6392bc3430eb559ce6a3c52b0253dbbb71d804be
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-11 12:53:49 +0700

    refactor: lint

--- src/components/Horo/HoroBgContainer.tsx
+++ src/components/Horo/HoroBgContainer.tsx
@@ -6,7 +6,7 @@ interface HoroBgContainerProps {
 
 const HoroBgContainer: React.FC<HoroBgContainerProps> = ({ children }) => {
   return (
-    <div className="flex w-full  md:w-10/12 flex-col items-center justify-center space-y-5 rounded-2xl bg-accent-purple p-9 shadow-inner">
+    <div className="flex w-full  flex-col items-center justify-center space-y-5 rounded-2xl bg-accent-purple p-9 shadow-inner md:w-10/12">
       {children}
     </div>
   );
--- src/components/Horo/SelectCard.tsx
+++ src/components/Horo/SelectCard.tsx
@@ -1,7 +1,7 @@
 import HoroCardBack from "@/assets/horo/card-cover.png";
 import HoroCard from "@/components/Card/HoroCard";
-import { Icon } from "@iconify/react";
 import Divider from "@/components/Divider";
+import { Icon } from "@iconify/react";
 
 interface SelectCardProps {
   selectedCard: number | null;
@@ -16,25 +16,25 @@ const SelectCard = ({
 }: SelectCardProps) => {
   return (
     <>
-     <div className="flex items-center justify-center space-x-3 md:hidden">
-          <Icon icon="custom:md:line" />
-          <Divider variant="accent-yellow" />
-          <Icon icon="custom:md:line" />
+      <div className="flex items-center justify-center space-x-3 md:hidden">
+        <Icon icon="custom:md:line" />
+        <Divider variant="accent-yellow" />
+        <Icon icon="custom:md:line" />
       </div>
-    <h2 className="text-white text-2xl font-bold">เลือกไพ่ 1 ใบเพื่อทำนาย</h2>
-    <div className="grid grid-cols-3 gap-3">
-      {cardList.map((cardImage: string, index: number) => (
-        <HoroCard
-          key={index}
-          image={selectedCard === index ? cardImage : HoroCardBack}
-          onClick={handleClick}
-        />
-      ))}
-    </div>
-    <div className="flex items-center justify-center space-x-3 md:hidden">
-          <Icon icon="custom:md:line" />
-          <Divider variant="accent-yellow" />
-          <Icon icon="custom:md:line" />
+      <h2 className="text-2xl font-bold text-white">เลือกไพ่ 1 ใบเพื่อทำนาย</h2>
+      <div className="grid grid-cols-3 gap-3">
+        {cardList.map((cardImage: string, index: number) => (
+          <HoroCard
+            key={index}
+            image={selectedCard === index ? cardImage : HoroCardBack}
+            onClick={handleClick}
+          />
+        ))}
+      </div>
+      <div className="flex items-center justify-center space-x-3 md:hidden">
+        <Icon icon="custom:md:line" />
+        <Divider variant="accent-yellow" />
+        <Icon icon="custom:md:line" />
       </div>
     </>
   );
