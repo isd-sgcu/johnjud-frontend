69d831b9fb60051ce9029bceb18dc97030956ed4
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-12 13:36:24 +0700

    feat: useMemo and add card number 10(forgot to add)

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,20 +1,27 @@
 import { Icon } from "@iconify/react";
-import React from "react";
+import React, { useMemo } from "react";
 
 interface DividerProps {
   variant: "primary" | "accent-yellow";
 }
 
 const Divider: React.FC<DividerProps> = ({ variant }) => {
-  const getClassName = () => {
-    return variant === "primary" ? "text-primary" : "text-accent-yellow";
-  };
+  const getClassName = useMemo(() => {
+    switch (variant) {
+      case "primary":
+        return "text-primary-500";
+      case "accent-yellow":
+        return "text-yellow-500";
+      default:
+        return "text-primary-500";
+    }
+  }, [variant]);
 
   return (
     <div className="flex items-center justify-center">
       <Icon
         icon="ph:paw-print"
-        className={`h-6 w-6 ${getClassName()} md:h-9 md:w-9`}
+        className={`h-6 w-6 ${getClassName} md:h-9 md:w-9`}
       />
     </div>
   );
