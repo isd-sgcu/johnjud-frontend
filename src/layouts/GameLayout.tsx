2b730ee948957dbb69829ecba2ad77c5e6e041ef
Zenith <sqoulo11@gmail.com>
2024-01-12 13:22:48 +0700

    refactor: add GameLayout bg color

--- src/layouts/GameLayout.tsx
+++ src/layouts/GameLayout.tsx
@@ -1,4 +1,3 @@
-import background from "@/assets/background/background.png";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
@@ -9,10 +8,7 @@ type GameLayoutProps = {
 const GameLayout = ({ children }: GameLayoutProps) => {
   return (
     <div
-      style={{
-        backgroundImage: `url(${background})`,
-      }}
-      className="flex min-h-screen flex-col bg-auto bg-top"
+      className="flex min-h-screen flex-col bg-auto bg-top bg-primary-variant"
     >
       <Navbar />
       <div>{children}</div>
