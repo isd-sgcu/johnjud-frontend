2ab1ebd1769d7a230eca9b452bf001009ca6a202
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-01-14 14:03:20 +0700

    fix: page's structure

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,27 +4,27 @@ import GameLayout from "@/layouts/GameLayout";
 
 const favourite = () => {
   return (
-    <Container>
-      <div className="flex flex-col items-center gap-6 py-6 lg:gap-10 lg:py-10">
+    <>
+      <Container className="flex flex-col items-center gap-6 py-6 lg:gap-10 lg:py-10">
         <div className="flex flex-col items-center gap-2">
           <div className="text-xl">
             Welcome,&nbsp;
             <span className="font-bold">This is JohnJud!</span>
           </div>
         </div>
-        <Divider></Divider>
-        <div>
-          <div className="flex flex-row items-center justify-center gap-4">
-            <div className="h-0.5 w-[100px] rounded-lg bg-primary"></div>
-            <div className="font-semibold text-primary lg:text-xl">
-              สัตว์เลี้ยงที่ถูกใจ
-            </div>
-            <div className="h-0.5 w-[100px] rounded-lg bg-primary"></div>
+      </Container>
+      <Divider></Divider>
+      <Container className="flex flex-col items-center gap-6 py-6 lg:gap-10 lg:py-10">
+        <div className="flex flex-row items-center justify-center gap-4">
+          <div className="h-0.5 w-[100px] rounded-lg bg-primary"></div>
+          <div className="font-semibold text-primary lg:text-xl">
+            สัตว์เลี้ยงที่ถูกใจ
           </div>
+          <div className="h-0.5 w-[100px] rounded-lg bg-primary"></div>
         </div>
         <div>-- PET CARD TO BE IMPLEMENTED --</div>
-      </div>
-    </Container>
+      </Container>
+    </>
   );
 };
 
