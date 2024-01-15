6bfeac22e2404e589f3d6dd7819bfaaab3532449
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-15 11:04:05 +0700

    refactor: lint and build failed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,7 +13,7 @@ const favourite = () => {
           </div>
         </div>
       </Container>
-      <Divider></Divider>
+      <Divider variant="primary"></Divider>
       <Container className="flex flex-col items-center gap-6 py-6 lg:gap-10 lg:py-10">
         <div className="flex flex-row items-center justify-center gap-4">
           <div className="h-0.5 w-[100px] rounded-lg bg-primary"></div>
