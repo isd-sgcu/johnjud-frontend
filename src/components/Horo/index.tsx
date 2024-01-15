018459d5a8955d5c1fcefbe86a2ab05d6479b400
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-01-15 13:42:00 +0700

    fix: where are you senoir

--- src/components/Horo/index.tsx
+++ src/components/Horo/index.tsx
@@ -38,7 +38,7 @@ const HoroGame = () => {
     switch (phase) {
       case 0:
         return (
-          <div className="-my-10 flex h-full w-full flex-col items-center gap-6 rounded-3xl bg-accent-purple pb-14 pt-8 lg:h-[650px] lg:w-[1024px] lg:gap-3 lg:pb-0">
+          <div className=" flex h-full w-full flex-col items-center gap-6 rounded-3xl bg-accent-purple pb-14 pt-8 lg:h-[650px] lg:w-[1024px] lg:gap-3 lg:pb-0">
             {/* className="flex h-[664px] w-80 flex-col items-center justify-center rounded-3xl bg-accent-purple" */}
             <div className="flex flex-col items-center gap-[6px] lg:gap-0">
               <div className="font-bold text-white">ยินดีต้อนรับเข้าสู่</div>
