4573c5f519f419b3ddef794e42407573cbf16af9
Zenith <sqoulo11@gmail.com>
2024-01-15 23:38:10 +0700

    feat: create game component from story data

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,12 +2,35 @@ import GamePage from "@/components/Game/GamePage";
 import GameLayout from "@/layouts/GameLayout";
 import { useEffect, useState } from "react";
 import Storys from "@/components/Game/Story";
-import Button from "@/components/Button";
 import StoryCard from "@/components/Game/StoryCard";
 import MultipleChoice from "@/components/Game/MultipleChoice";
 
 const userTarotFirstpage = () => {
+  const [start, setStart] = useState(false);
   const [currentComponent, setCurrentComponent] = useState(<GamePage onClick={() => setStart(true)} />);
+  const [storyIdx, setStoryIdx] = useState(0);
+
+  useEffect(() => {
+    if (start) {
+      async function showStoryCards() {
+        for (const story of Storys[0].storyTexts) {
+          await new Promise((resolve) => {
+            setCurrentComponent(<StoryCard text={story} />);
+            setTimeout(resolve, 5000);
+          });
+        }
+      }
+
+      showStoryCards().then(() => {
+        window.scrollTo(0,0);
+        setCurrentComponent(<MultipleChoice
+          index={storyIdx + 1}
+          headerText={Storys[storyIdx].headerText}
+          answers={Storys[storyIdx].answers}
+        />);
+      });
+    }
+  }, [start, storyIdx]);
   return (
     <>
       {currentComponent}
