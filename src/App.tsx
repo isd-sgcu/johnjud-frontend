e41a3b8bc7fab959322fd4bf4346330478702549
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2024-01-15 11:09:47 +0700

    Feat: ScrollToTop
    Still have a problem. White screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function AppRoot() {
   return (
@@ -19,6 +20,7 @@ function AppRoot() {
 function App() {
   return (
     <StrictMode>
+      <ScrollToTop />
       <AppRoot />
     </StrictMode>
   );
--- src/utils/ScrollToTop.ts
+++ src/utils/ScrollToTop.ts
@@ -1,18 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export default function ScrollToTop() {
-  const { pathname } = useLocation();
-  console.log("Work");
+const ScrollToTop = () => {
+  const location = useLocation();
 
   useEffect(() => {
-    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth", // Optional if you want to skip the scrolling animation
-    });
-  }, [pathname]);
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return null;
-}
+};
+
+export default ScrollToTop;
