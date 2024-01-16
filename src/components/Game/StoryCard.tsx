df26157a72caf7e14d4be7305d5c97ddd280ce8f
Zenith <sqoulo11@gmail.com>
2024-01-16 20:31:23 +0700

    feat: text enter new line on \n

--- src/components/Game/StoryCard.tsx
+++ src/components/Game/StoryCard.tsx
@@ -9,7 +9,7 @@ const StoryCard = (props: StoryCardProps) => {
     <div className="flex min-h-screen w-full flex-col items-center justify-center px-6 py-6 md:py-10">
       <div className="flex h-full max-h-[32rem] w-full max-w-3xl flex-grow flex-col items-center justify-center gap-6 rounded-2xl bg-white">
         <img src={phoneIcon} alt="phoneIcon" className="h-24 w-24" />
-        <div className="w-full justify-center break-words px-6 text-center text-xl font-bold md:text-2xl">
+        <div className="w-full justify-center break-words px-6 text-center text-xl font-bold md:text-2xl whitespace-pre-line">
           {props.text}
         </div>
       </div>
