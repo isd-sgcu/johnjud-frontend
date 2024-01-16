e38e5d3a763bf39b0397707a485fe7b8b1844d37
Zenith <sqoulo11@gmail.com>
2024-01-16 21:24:07 +0700

    feat: score system for each choice

--- src/components/Game/MultipleChoice.tsx
+++ src/components/Game/MultipleChoice.tsx
@@ -4,7 +4,7 @@ interface MultipleChoiceProps {
   index: number;
   headerText: string;
   answers: string[];
-  onClick: () => void;
+  onClick: (index: number) => void;
 }
 
 const MultipleChoice = (props: MultipleChoiceProps) => {
@@ -35,7 +35,7 @@ const MultipleChoice = (props: MultipleChoiceProps) => {
                 <div
                   key={index}
                   className="flex w-full cursor-pointer items-center justify-center break-words rounded-3xl bg-accent-red px-6 py-2 text-center font-semibold text-white md:text-lg"
-                  onClick={props.onClick}
+                  onClick={() => props.onClick(index)}
                 >
                   {ans}
                 </div>
