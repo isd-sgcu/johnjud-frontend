32e44c97be4bcb0d4b4d003e62f5e85262d25f02
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2024-01-16 11:26:28 +0700

    Fix: rename

--- src/utils/ScrollToTop.ts
+++ src/utils/ScrollToTop.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const scrollToTop = () => {
+const ScrollToTop = () => {
   const location = useLocation();
 
   useEffect(() => {
@@ -11,4 +11,4 @@ const scrollToTop = () => {
   return null;
 };
 
-export default scrollToTop;
+export default ScrollToTop;
