0f946b8ee889225e6389221fe79703ad2e3365c9
boomchanotai <boom9387@hotmail.com>
2024-01-16 17:21:13 +0700

    feat: add pets api

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -1,9 +1,12 @@
+import PetCard from "@/components/Card/PetCard";
 import Container from "@/components/Container";
 import Filter from "@/components/Filter";
 import { PetIcon } from "@/components/Filter/Icon";
 import Heading from "@/components/Pets/Heading";
 import PetSearch from "@/components/Search/PetSearch";
+import { usePetsQuery } from "@/hooks/queries/usePetsQuery";
 import MainLayout from "@/layouts/MainLayout";
+import { Pet } from "@/types/pets";
 import { useCallback, useState } from "react";
 
 const Pets = () => {
@@ -12,6 +15,8 @@ const Pets = () => {
   const toggleIsOpenFilterPanel = useCallback(() => {
     setIsOpenFilterPanel((prev) => !prev);
   }, []);
+
+  const { data } = usePetsQuery();
 
   return (
     <>
@@ -29,7 +34,26 @@ const Pets = () => {
         </div>
       </Container>
       <Container>
-        <div>{/* Pet Card */}</div>
+        <div className="flex flex-wrap items-center justify-center gap-6 lg:gap-9">
+          {data?.pets.map((pet: Pet) => (
+            <PetCard
+              key={pet.id}
+              id={pet.id}
+              image={
+                "https://f.ptcdn.info/251/076/000/r6phkmmxuGNF1erTdMG-o.jpg"
+              }
+              type={pet.type}
+              name={pet.name}
+              status={pet.status}
+              gender={pet.gender}
+              birthDate={pet.birthdate}
+              habit={pet.habit}
+              isSterile={pet.is_sterile}
+              isLiked={false}
+              isVisibled={pet.is_visible}
+            />
+          ))}
+        </div>
       </Container>
     </>
   );
