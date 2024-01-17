0718b776bc38c014046b950630a833c9dd5f091f
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-17 13:43:23 +0700

    refactor: lint

--- src/components/Main/CardList/MiniPetCardContainer.tsx
+++ src/components/Main/CardList/MiniPetCardContainer.tsx
@@ -1,24 +1,24 @@
+import MiniPetCard from "@/components/Card/MiniPetCard";
 import { Pet } from "@/types/pets";
 import React from "react";
-import MiniPetCard from "@/components/Card/MiniPetCard";
 interface MiniPetCardContainerProps {
-    petsData : Pet[];
+  petsData: Pet[];
 }
 
-
-const MiniPetCardContainer: React.FC<MiniPetCardContainerProps> = ({ petsData }) => {
-  
+const MiniPetCardContainer: React.FC<MiniPetCardContainerProps> = ({
+  petsData,
+}) => {
   return (
     <div className="grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-6">
       {petsData?.map((pet: Pet) => (
         <MiniPetCard
           key={pet.id}
           id={pet.id}
-          name = {pet.name}
+          name={pet.name}
           species={pet.type}
           gender={pet.gender}
           birthdate={pet.birthdate}
-          />  
+        />
       ))}
     </div>
   );
