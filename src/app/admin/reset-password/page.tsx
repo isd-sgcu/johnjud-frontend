f3a2b957e3ae1f522385c3208ba0e31eac41742c
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-01-18 08:16:53 +0700

    fix: move file to /admin

---
+++ src/app/admin/reset-password/page.tsx
@@ -0,0 +1,13 @@
+import Button from "@/components/Button";
+import Container from "@/components/Container";
+import MainLayout from "@/layouts/MainLayout";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+const resetPassword = () => {
+  return (
+  );
+};
+
+export default resetPassword;
+
+export const Layout = MainLayout;
