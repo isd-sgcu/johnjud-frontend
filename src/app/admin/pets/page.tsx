fce9ddf2db8b40a4b7eed297fb72f928c4824f02
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-19 13:06:05 +0700

    feat: add meta.total to admin/pets

--- src/app/admin/pets/page.tsx
+++ src/app/admin/pets/page.tsx
@@ -30,7 +30,7 @@ const Pets = () => {
         </Link>
       </div>
       <Container>
-        <Heading onSearch />
+        <Heading onSearch quantity={data?.meta.total}/>
       </Container>
       <Container className="flex flex-col items-center space-y-6">
         <div className="flex w-full flex-row gap-x-4">
