dfb9a326e3476270b169d2d0804830ba5a017d81
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-26 00:48:09 +0700

    feat: add auth api function

---
+++ src/api/auth/password/resetPassword.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+interface resetPasswordResponse {
+    is_success: boolean;
+}
+
+const resetPassword = async (password: string, token: string) => {
+    const response = await axios.post<resetPasswordResponse>(
+        `${import.meta.env.VITE_API_URL}/auth/password/resetPassword`,
+        { password, token }
+    );
+    
+    return response.data;
+}
+
+export { resetPassword }
+export type { resetPasswordResponse }
---
+++ src/api/auth/signUp.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+interface SignUpResponse {
+    email : string;
+    password : string;
+    firstname : string;
+    lastname : string;
+}
+
+const signUp = async (email: string, firstname: string, lastname: string, password: string) => {
+    const response = await axios.post<SignUpResponse>(
+        `${import.meta.env.VITE_API_URL}/auth/signup`,
+        { email, firstname, lastname, password }
+    );
+    
+    return response.data;
+}
+
+export { signUp }
+export type { SignUpResponse }
