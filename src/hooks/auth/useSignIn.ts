da4bdcf2971dd435115a34131c888dbdfefe0ae0
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-01-31 23:56:02 +0700

    feat: redirect to /admin/pet after login finsh

--- src/hooks/auth/useSignIn.ts
+++ src/hooks/auth/useSignIn.ts
@@ -1,10 +1,11 @@
 import { signIn, SignInCredentials, SignInResponse } from "@/api/auth/signIn";
 import useAuthStore from "@/store/authStore";
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
+import { useNavigate } from 'react-router-dom';
 
 const useSignIn = () => {
   const { setAuth } = useAuthStore();
-
+    const navigate = useNavigate();
   const mutationOptions: UseMutationOptions<
     SignInResponse,
     Error,
@@ -14,6 +15,7 @@ const useSignIn = () => {
       signIn(credentials.email, credentials.password),
     onSuccess: (data: SignInResponse) => {
       setAuth(data.access_token, data.refresh_token);
+        navigate('/admin/pets');
     },
     onError: (error: Error) => {
       console.log(error);
