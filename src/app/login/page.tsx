8cebd3f9a4c3ffc2b75322f718333ab39213815b
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-02-01 21:36:12 +0700

    fix: refrest token gone : use persist

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,31 @@
 import LoginForm from "@/components/Form/LoginForm";
 import useSignIn from "@/hooks/auth/useSignIn";
 import MainLayout from "@/layouts/MainLayout";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useAuthStore from "@/store/authStore";
+import { useNavigate } from "react-router-dom";
 
 // Page
 const Login = () => {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const signIn = useSignIn();
+  const navigate = useNavigate();
+  const { isLoggedIn } = useAuthStore();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     signIn.mutate({ email, password });
   };
-
+  console.log("isLoggedIn", isLoggedIn)
+  // if the user logged in and we in /login redirect to /home
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/home");
+    }
+  }, [isLoggedIn, navigate]);
+  
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center space-y-12 p-12">
       <div className="flex flex-col items-center justify-center space-y-4 px-3">
