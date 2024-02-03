fe437696af98098ab23f524b0a8ab7c4abe865bb
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-02-03 21:21:56 +0700

    refactor: delete /login

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,24 +1,51 @@
-import MenuButtton from "@/components/Admin/MenuButton";
-import Container from "@/components/Container";
+import LoginForm from "@/components/Form/LoginForm";
+import useSignIn from "@/hooks/auth/useSignIn";
 import MainLayout from "@/layouts/MainLayout";
+import useAuthStore from "@/store/authStore";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Admin = () => {
+// Page
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = useSignIn();
+  const navigate = useNavigate();
+  const { isLoggedIn } = useAuthStore();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    signIn.mutate({ email, password });
+  };
+  console.log("isLoggedIn", isLoggedIn);
+  // if the user logged in and we in /login redirect to /home
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/home");
+    }
+  }, [isLoggedIn, navigate]);
+
   return (
-    <Container className="space-y-8 py-16">
-      <h2 className="text-center text-lg">
-        Welcome back, <span className="font-bold">admin</span>
-      </h2>
-      <div className="flex flex-row items-center justify-center gap-16">
-        <MenuButtton
-          link="/admin/pets"
-          icon="ph:paw-print"
-          text="Pets Manager"
-        />
+    <div className="flex min-h-screen w-full flex-col items-center justify-center space-y-12 p-12">
+      <div className="flex flex-col items-center justify-center space-y-4 px-3">
+        <h1 className="text-nowrap text-5xl font-bold text-primary ">
+          เข้าสู่ระบบ
+        </h1>
+        <p className="text-johnjud-gray text-nowrap text-lg font-normal">
+          เข้าสู่ระบบของคุณ
+        </p>
       </div>
-    </Container>
+      <LoginForm
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+      />
+    </div>
   );
 };
 
-export default Admin;
+export default Login;
 
+// Layout
 export const Layout = MainLayout;
+MainLayout;
