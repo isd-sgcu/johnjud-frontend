b5e7e90d7c6bf9ea28d23be03267ae431e5bfbae
Zenith <sqoulo11@gmail.com>
2024-02-04 02:08:44 +0700

    refactor: api path

--- src/hooks/mutation/useCreatePet.ts
+++ src/hooks/mutation/useCreatePet.ts
@@ -1,4 +1,4 @@
-import { petCreateRequest, postPetCreate } from "@/api/createPets";
+import { petCreateRequest, postPetCreate } from "@/api/pets";
 import { useMutation } from "@tanstack/react-query";
 
 const useCreatePet = () => {
