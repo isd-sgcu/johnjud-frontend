68036cb554ff28b7a500035e90bf537fbf0fdc1b
Michael Scofield <135577456+MichaelScodaeng@users.noreply.github.com>
2024-02-06 14:15:18 +0700

    Refactor:code

--- src/store/favStore.ts
+++ src/store/favStore.ts
@@ -1,26 +1,28 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
+// Define the store state
 interface StoreState {
   favorites: string[];
   addToFavorites: (petId: string) => void;
   removeFromFavorites: (petId: string) => void;
 }
 
+// Create the store
 const useStore = create<StoreState>(
   persist(
     (set) => ({
       favorites: [],
       addToFavorites: (petId: string) =>
         set((state) => ({ favorites: [...state.favorites, petId] })),
-      removeFromFavorites: (petId) =>
+      removeFromFavorites: (petId: string) =>
         set((state) => ({
           favorites: state.favorites.filter((id) => id !== petId),
         })),
     }),
     {
-      name: "petStore",
-      getStorage: () => localStorage,
+      name: 'favoritesStore', // Name for the localStorage key
+      getStorage: () => localStorage, // Use localStorage for persistence
     }
   )
 );
