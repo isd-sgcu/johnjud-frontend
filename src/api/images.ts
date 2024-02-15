ec12b500baea0f7e3fe184c907a498fa57eb3495
Zenith <sqoulo11@gmail.com>
2024-02-15 21:35:34 +0700

    feat: working api now

--- src/api/images.ts
+++ src/api/images.ts
@@ -8,16 +8,21 @@ type postImageRequest = {
 
 interface postImageResponse extends Image {}
 
-const token: string = "";
-
+const token: string =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2pvaG5qdWQuaXNkLnNnY3UuaW4udGgiLCJleHAiOjE3MDgwMDk3NDQsImlhdCI6MTcwODAwNjE0NCwidXNlcl9pZCI6ImM0YTI1OTFlLTZjNWEtNDliNS05NDhkLTU0MDJkMjcxNGZlMiIsImF1dGhfc2Vzc2lvbl9pZCI6IjY4NDE2M2FkLWJkYWUtNDlhMS1hNTkyLWRkMjU4NjU0MThkMyJ9.u39pVKLTzl6FKQNDNDhghknxkaf9x3pP_3zD2oE_Fes";
 const postImage = async (
   data: postImageRequest
 ): Promise<postImageResponse> => {
+  const formData = new FormData();
+  formData.append("file", data.file);
+  if (data.pet_id) formData.append("pet_id", data.pet_id);
+
   const response = await axios.post<postImageResponse>(
     `${import.meta.env.VITE_API_URL}/images`,
-    data,
+    formData,
     {
       headers: {
+        "Context-Type": "multipart/form-data",
         Authorization: `Bearer ${token}`,
       },
     }
