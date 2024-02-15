b664e6497aa1bcd509768f52e75786c4f75ff5c5
Zenith <sqoulo11@gmail.com>
2024-02-15 21:35:00 +0700

    refactor: change hooks name

--- src/hooks/mutation/usePostPet.ts
+++ src/hooks/mutation/usePostPet.ts
@@ -1,9 +1,9 @@
-import { petCreateRequest, postPetCreate } from "@/api/pets";
+import { postPet, postPetRequest } from "@/api/pets";
 import { useMutation } from "@tanstack/react-query";
 
 const useCreatePet = () => {
   return useMutation({
-    mutationFn: (data: petCreateRequest) => postPetCreate(data),
+    mutationFn: (data: postPetRequest) => postPet(data),
   });
 };
 
