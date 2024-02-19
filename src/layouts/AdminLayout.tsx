815c3002fc593202602d2b77afbc507b7dd58ecf
boomchanotai <boom9387@hotmail.com>
2024-02-19 23:11:26 +0700

    feat: add return route

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -24,7 +24,7 @@ const AdminLayout = ({ children }: MainLayoutProps) => {
     if (isLoggedIn && refreshToken) {
       mutate({ refresh_token: refreshToken });
     }
-  }, []);
+  }, [isLoggedIn]);
 
   return (
     <>
