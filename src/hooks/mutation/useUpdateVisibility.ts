95faef2fce445fae4867b39a7de4e86e38e5751d
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-19 12:18:26 +0700

    feat: using updateVisibility with petcard

--- src/hooks/mutation/useUpdateVisibility.ts
+++ src/hooks/mutation/useUpdateVisibility.ts
@@ -1,10 +1,17 @@
 import { updateVisibility } from "@/api/pets";
 import { useMutation } from "@tanstack/react-query";
+import toast from 'react-hot-toast'
 
 const useUpdateVisibility = () => {
   return useMutation({
     mutationFn: ({ id, visibility }: { id: string; visibility: boolean }) =>
       updateVisibility(id, visibility),
+    onSuccess: () => {
+      toast.success("เปลี่ยนการมองเห็นสำเร็จ")
+    },
+    onError: () => {
+      toast.error("มีบางอย่างผิดพลาด")
+    }
   });
 };
 
