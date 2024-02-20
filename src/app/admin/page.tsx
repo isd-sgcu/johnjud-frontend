84b3804f6594ec640dfce641211470990c35b914
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-20 11:03:35 +0700

    fix: just kidding

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,11 +20,11 @@ const Login = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center space-y-12 p-12">
       <div className="flex flex-col items-center justify-center space-y-4 px-3">
-        <h1 className="text-nowrap text-5xl font-bold text-primary ">
-          เข้าสู่ระบบ
+        <h1 className="text-nowrap text-5xl font-bold text-accent-red">
+          ควยยย
         </h1>
         <p className="text-johnjud-gray text-nowrap text-lg font-normal">
-          เข้าสู่ระบบของคุณ
+          ควยยย
         </p>
       </div>
       <LoginForm mutation={mutate} />
