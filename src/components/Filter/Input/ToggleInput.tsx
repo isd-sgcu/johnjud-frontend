7907f57233fc664284060eb11f2d8c1feb50723b
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-02-20 15:06:01 +0700

    feat: change to tick

--- src/components/Filter/Input/ToggleInput.tsx
+++ src/components/Filter/Input/ToggleInput.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@iconify/react";
+
 interface ToggleButtonProps {
   id: string;
   text: string;
@@ -13,8 +15,16 @@ const ToggleInput = ({ id, text, checked, onChange }: ToggleButtonProps) => {
     >
       <span>{text}</span>
       <div className="flex h-6 w-6 flex-col items-center justify-center rounded-full bg-white">
-        <input type="checkbox" id={id} className="peer sr-only" checked ={checked} onChange={onChange}/>
-        <div className="h-4 w-4 rounded-full transition-all duration-150 peer-checked:bg-accent-red peer-checked:bg-opacity-80"></div>
+        <input
+          type="checkbox"
+          id={id}
+          className="peer sr-only"
+          checked={checked}
+          onChange={onChange}
+        />
+        <div className="h-5 w-5 rounded-full transition-all duration-150 peer-checked:bg-accent-red peer-checked:bg-opacity-80">
+          <Icon icon="bi:check" className="h-5 w-5 text-white" />
+        </div>
       </div>
     </label>
   );
