4ec12030131793a89f421d06c1cf012b89ebb37a
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-02-20 15:16:03 +0700

    feat: move state to the app/pets/ page

---
+++ src/types/filter.ts
@@ -0,0 +1,15 @@
+interface filterState {
+    dog: boolean;
+    cat: boolean;
+    male: boolean;
+    female: boolean;
+    white: boolean;
+    black: boolean;
+    brown: boolean;
+    blonde: boolean;
+    minAge: number;
+    maxAge: number;
+}
+
+
+export type {filterState};
