57330ed3f0bee6de6d00cce458870a67d3813193
boomchanotai <boom9387@hotmail.com>
2024-02-20 17:24:10 +0700

    fix: seo

--- src/hooks/usePathStyle.ts
+++ src/hooks/usePathStyle.ts
@@ -3,33 +3,24 @@ import { useLocation } from "react-router-dom";
 import logoPrimary from "../assets/navbar/logoPrimary.webp";
 import logoSecondary from "../assets/navbar/logoSecondary.svg";
 
-type PathStyle = {
-  color: string;
-  logo: string;
-  theme: "primary" | "accent-red" | "accent-purple";
-};
-
 const usePathStyle = () => {
   const pathname = useLocation().pathname;
 
-  const style: PathStyle = useMemo(() => {
+  const style = useMemo(() => {
     if (pathname.includes("/admin")) {
       return {
         color: "bg-accent-red",
         logo: logoPrimary,
-        theme: "accent-red",
       };
     } else if (pathname.includes("/horo")) {
       return {
         color: "bg-accent-purple",
         logo: logoSecondary,
-        theme: "accent-purple",
       };
     } else {
       return {
         color: "bg-primary",
         logo: logoPrimary,
-        theme: "primary",
       };
     }
   }, [pathname]);
