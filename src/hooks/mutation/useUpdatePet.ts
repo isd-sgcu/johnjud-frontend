67fce22f9a704cc19c9bb34682ae5ac40e439061
teegoood <teegooodgood@gmail.com>
2024-02-20 20:19:30 +0700

    add: toast

--- src/hooks/mutation/useUpdatePet.ts
+++ src/hooks/mutation/useUpdatePet.ts
@@ -1,5 +1,6 @@
 import { PutPetRequest, updatePet } from "@/api/pets";
 import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 interface UpdateMutationParameter {
   body: PutPetRequest;
@@ -10,6 +11,12 @@ function useUpdatePet() {
   return useMutation({
     mutationFn: (data: UpdateMutationParameter) => {
       return updatePet(data.body, data.id);
+    },
+    onSuccess: () => {
+      toast.success("เเก้ไขข้อมูลสัตว์เลี้ยงสำเร็จ");
+    },
+    onError: () => {
+      toast.error("มีบางอย่างผิดพลาด");
     },
   });
 }
