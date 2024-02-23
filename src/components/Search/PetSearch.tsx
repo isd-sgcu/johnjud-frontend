519a76a7eb04ac1388ed1d106858aa8d4cf53bf2
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-02-23 23:28:56 +0700

    feat: add search function

--- src/components/Search/PetSearch.tsx
+++ src/components/Search/PetSearch.tsx
@@ -1,14 +1,32 @@
+import { filterState } from "@/types/filter";
 import { Icon } from "@iconify/react";
-import React from "react";
+import React, { useEffect } from "react";
 /*
   for pets page use : @custom:md:bubble-red
   for home page use: @custom:md:bubble-green
 */
 interface PetSearchProps {
   variant: "red" | "green";
+  filter: filterState;
+  setFilters: React.Dispatch<React.SetStateAction<filterState>>
 }
 
-const PetSearch: React.FC<PetSearchProps> = ({ variant }) => {
+const PetSearch: React.FC<PetSearchProps> = ({
+  variant,
+  filter,
+  setFilters,
+}) => {
+
+  const [inputValue, setInputValue] = React.useState<string>(filter.search || "");
+
+  // add debouce time for delay 
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      setFilters({ ...filter, search: inputValue });
+    }, 500);
+    return () => clearTimeout(delayDebounce);
+  }, [inputValue, setFilters, filter]);
+
   return (
     <div className="flex w-full flex-row items-center justify-start gap-3 rounded-full bg-white px-6 py-3 shadow">
       <div>
@@ -18,6 +36,8 @@ const PetSearch: React.FC<PetSearchProps> = ({ variant }) => {
         type="text"
         placeholder="ค้นหาสัตว์เลี้ยง"
         className="w-full py-0 outline-none"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       />
     </div>
   );
