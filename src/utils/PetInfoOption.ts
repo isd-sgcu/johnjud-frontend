5376e0eedf636e4044e937c3364b454c6196ea29
Sirawit Chanaburanasak <97283645+punpunkeshin05@users.noreply.github.com>
2024-04-28 14:19:48 +0700

    feat: more color on create and update pets

--- src/utils/PetInfoOption.ts
+++ src/utils/PetInfoOption.ts
@@ -39,6 +39,50 @@ const colorOption: OptionType[] = [
     label: "บลอนด์",
     value: "blonde",
   },
+  {
+    label: "สลิด",
+    value: "tabby",
+  },
+  {
+    label: "ขาวดำ",
+    value: "whiteBlack",
+  },
+  {
+    label: "ขาวส้ม",
+    value: "whiteOrange",
+  },
+  {
+    label: "เปรอะ",
+    value: "tortoiseShell",
+  },
+  {
+    label: "สามสี",
+    value: "calico",
+  },
+  {
+    label: "เทา",
+    value: "gray",
+  },
+  {
+    label: "ครีม",
+    value: "cream",
+  },
+  {
+    label: "แต้มน้ำตาล",
+    value: "brownSpots",
+  },
+  {
+    label: "แต้มเทา",
+    value: "greySpots",
+  },
+  {
+    label: "แต้มส้ม",
+    value: "orangeSpots",
+  },
+  {
+    label: "แต้มเปรอะ",
+    value: "dirtySpots"
+  }
 ];
 
 export { colorOption, genderOption, typeOption };
