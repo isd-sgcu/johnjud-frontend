b190007e25f76e7373e72bab95e1c827fa31e6b1
Chanotai Krajeam <boom9387@hotmail.com>
2024-05-17 17:15:00 +0700

    refactor: auth refreshtoken (#100)
    * feat: refresh Token
    
    * refactor: refresh token

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { StrictMode, Suspense } from "react";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { StrictMode, Suspense, useState } from "react";
 import { RouterProvider } from "react-router-dom";
+import { refreshToken as getRefreshToken } from "./api/auth/refreshToken";
 import { router } from "./routes";
+import useAuthStore from "./store/authStore";
+import { calculateExpiryTime } from "./utils/calculateExpiryTime";
 
 function AppRoot() {
   return (
@@ -18,7 +25,65 @@ function AppRoot() {
 }
 
 function App() {
-  const queryClient = new QueryClient();
+  const accessToken = useAuthStore((state) => state.accessToken);
+  const refreshToken = useAuthStore((state) => state.refreshToken);
+  const setAuth = useAuthStore((state) => state.setAuth);
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const [isRefreshingToken, setIsRefreshingToken] = useState(false);
+
+  const refreshAuthToken = async (
+    accessToken: string,
+    refreshToken: string
+  ) => {
+    if (isRefreshingToken) return;
+    if (accessToken == null || refreshToken == null) return;
+
+    try {
+      const response = await getRefreshToken(refreshToken);
+
+      const expriedAt = calculateExpiryTime(response.expires_in);
+      setAuth(response.access_token, response.refresh_token, expriedAt);
+    } catch (error) {
+      clearAuth();
+      router.navigate("/admin");
+    } finally {
+      setIsRefreshingToken(false);
+    }
+  };
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 30000, // 30 seconds
+            retry: (failureCount, error) => {
+              // Don't retry for certain error responses
+              if (
+                error.message.includes("400") ||
+                error.message.includes("401")
+              ) {
+                return false;
+              }
+
+              // Retry others just once
+              return failureCount <= 1;
+            },
+          },
+        },
+        queryCache: new QueryCache({
+          onError: (error) => {
+            if (
+              error.message.includes("400") ||
+              error.message.includes("401")
+            ) {
+              if (accessToken == null || refreshToken == null) return;
+              refreshAuthToken(accessToken, refreshToken);
+            }
+          },
+        }),
+      })
+  );
 
   return (
     <StrictMode>
