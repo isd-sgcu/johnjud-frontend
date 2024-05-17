6b4c4284c69631f9d24c914fd03c4479edb0b022
Chanotai Krajeam <boom9387@hotmail.com>
2024-05-17 17:15:00 +0700

    refactor: auth refreshtoken (#100)
    * feat: refresh Token
    
    * refactor: refresh token

--- src/api/auth/refreshToken.ts
+++ src/api/auth/refreshToken.ts
@@ -1,4 +1,3 @@
-import useAuthStore from "@/store/authStore";
 import axios from "axios";
 
 interface RefreshTokenResponse {
@@ -14,17 +13,10 @@ interface RefreshTokenCredentials {
 const refreshToken = async (
   refresh_token: string
 ): Promise<RefreshTokenResponse> => {
-  const { accessToken } = useAuthStore.getState();
-
   const response = await axios.post<RefreshTokenResponse>(
     `${import.meta.env.VITE_API_URL}/auth/refreshToken`,
     {
       refresh_token,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
     }
   );
 
